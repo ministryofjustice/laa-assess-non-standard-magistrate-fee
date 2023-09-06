7f1e0dee00fd346027e70c22922c0114fcc7a6fb
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-09-06 14:24:29 +0100

    Fix issues with db schema and govuk-frontend

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_144048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "claims", id: :uuid, default: -> { "public.gen_random_uuid()" }, force: :cascade do |t|
+  create_table "claims", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "state"
     t.string "risk"
     t.integer "current_version"
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_144048) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "versions", id: :uuid, default: -> { "public.gen_random_uuid()" }, force: :cascade do |t|
+  create_table "versions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "claim_id"
     t.integer "version"
     t.integer "json_schema_version"
