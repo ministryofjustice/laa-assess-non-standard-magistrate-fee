afbb819425eb72bd37ded6d0654c8082a73f985e
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-09-06 16:02:11 +0100

    add ping endpoint again

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "claims#index"
-
+  
+  get :ping, to: 'healthcheck#ping'
+  
   resources :healthcheck, only: [] do
     collection do
       get :ping
