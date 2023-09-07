7006e8e19ecf45d26b840f76401d70209cdc0f2b
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-09-07 12:45:38 +0100

    Add additional tests to healthcheck ping

--- app/controllers/healthcheck_controller.rb
+++ app/controllers/healthcheck_controller.rb
@@ -9,7 +9,11 @@ class HealthcheckController < ApplicationController
 
   def build_args
     {
-      hello: 'world'
+      hello: 'world',
+      branch_name: ENV.fetch('APP_BRANCH_NAME', nil),
+      build_date: ENV.fetch('APP_BUILD_DATE', nil),
+      build_tag: ENV.fetch('APP_BUILD_TAG', nil),
+      commit_id: ENV.fetch('APP_GIT_COMMIT', nil)
     }
   end
 end
