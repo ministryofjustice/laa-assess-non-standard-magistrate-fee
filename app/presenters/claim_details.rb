203220bd7b5573c44bb4035b170a1931e8f1d4ce
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-09-11 18:49:36 +0100

    Add defendant details section

--- app/presenters/claim_details.rb
+++ app/presenters/claim_details.rb
@@ -8,8 +8,8 @@ module ClaimDetails
 
     def table
       [
-        { title: I18n.t('.claim_details.details_of_claim.title'), data: details_of_claim_section }
-      #   {title: 'Defendant details', data: DetailsOfClaimSection },
+        { title: I18n.t('.claim_details.details_of_claim.title'), data: details_of_claim_section },
+        { title: I18n.t('.claim_details.defendant_details.title'), data: defendant_details_section },
       #   {title: 'Case details', data: DetailsOfClaimSection },
       #   {title: 'Case disposal', data: DetailsOfClaimSection },
       #   {title: 'Claim justification', data: DetailsOfClaimSection },
@@ -25,10 +25,56 @@ module ClaimDetails
     
     def details_of_claim_section
       [
-        {title: I18n.t('.claim_details.details_of_claim.ufn'), value: claim_details.ufn },
-        {title: I18n.t('.claim_details.details_of_claim.claim_type'), value: I18n.t(".claim_types.#{claim_details.claim_type}") },
-        {title: I18n.t('.claim_details.details_of_claim.rep_order_date'), value: ApplicationController.helpers.format_date_string(claim_details.rep_order_date) }
+        { 
+          title: I18n.t('.claim_details.details_of_claim.ufn'),
+          value: claim_details.ufn 
+        },
+        { 
+          title: I18n.t('.claim_details.details_of_claim.claim_type'),
+          value: I18n.t(".claim_types.#{claim_details.claim_type}") 
+        },
+        { 
+          title: I18n.t('.claim_details.details_of_claim.rep_order_date'),
+          value: ApplicationController.helpers.format_date_string(claim_details.rep_order_date) 
+        }
       ]
+    end
+
+    def defendant_details_section
+      defendant_rows.flatten
+    end
+
+    def defendant_rows
+      binding.pry
+      main_defendant_rows + additional_defendant_rows 
+    end
+
+    def main_defendant_rows
+      [
+        { 
+          title:  I18n.t('.claim_details.defendant_details.main_defendant_name'),
+          value: claim_details.main_defendant_name
+        },
+        { 
+          title: I18n.t('.claim_details.defendant_details.main_defendant_maat'),
+          value: claim_details.main_defendant_maat
+        }
+      ] 
+    end
+
+    def additional_defendant_rows
+      claim_details.additional_defendants.map.with_index do |defendant, index|
+        [
+          { 
+            title: I18n.t('.claim_details.defendant_details.defendant_name', count: index + 1),
+            value: defendant['full_name']
+          },
+          { 
+            title: I18n.t('.claim_details.defendant_details.defendant_maat' , count: index + 1),
+            value: defendant['maat']
+          }
+        ]
+      end  
     end
   end
 end
--- app/view_models/v1/claim_details.rb
+++ app/view_models/v1/claim_details.rb
@@ -3,5 +3,20 @@ module V1
     attribute :ufn
     attribute :claim_type
     attribute :rep_order_date
+    attribute :defendants
+
+    def main_defendant_name
+      main_defendant = defendants.detect { |defendant| defendant['main'] }
+      main_defendant ? main_defendant['full_name'] : ''
+    end
+
+    def main_defendant_maat
+      main_defendant = defendants.detect { |defendant| defendant['main'] }
+      main_defendant ? main_defendant['maat'] : ''
+    end
+
+    def additional_defendants
+      defendants.reject { |defendant| defendant['main'] == true }
+    end
   end
 end
