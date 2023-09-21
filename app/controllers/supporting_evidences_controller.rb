b4e5554834e2110cd96af4b345bb9dd95c7b3656
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2023-09-21 11:36:07 +0100

    Adds table view

--- app/controllers/supporting_evidences_controller.rb
+++ app/controllers/supporting_evidences_controller.rb
@@ -2,7 +2,7 @@ class SupportingEvidencesController < ApplicationController
   def show
     claim = Claim.find(params[:claim_id])
     claim_summary = BaseViewModel.build(:claim_summary, claim)
-
-    render locals: { claim:, claim_summary: }
+    supporting_evidence = BaseViewModel.build_all(:supporting_evidence, claim, 'supporting_evidences')
+    render locals: { claim:, claim_summary:, supporting_evidence: }
   end
 end
