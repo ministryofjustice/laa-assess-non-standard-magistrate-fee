b1bc052899fd79a14b03cd605ea22fdaf1f13711
David Henry <david.henry@digital.justice.gov.uk>
2023-09-21 15:06:18 +0100

    Update cost_calculator.rb

--- app/lib/cost_calculator.rb
+++ app/lib/cost_calculator.rb
@@ -1,11 +1,27 @@
 module CostCalculator
-  def self.cost(type, object)
-    case type
-    when :work_item
+  class << self
+    def cost(type, object)
+      case type
+      when :work_item
+        work_item_cost(object)
+      when :letter_and_call
+        letter_and_call_cost(object)
+      when :disbursement
+        disbursement_cost(object)      
+      end
+    end
+
+    private 
+    
+    def work_item_cost(object)
       object.pricing * object.time_spent * (100 + object.uplift.to_i) / 100 / 60
-    when :letter_and_call
+    end
+
+    def letter_and_call_cost(object)
       object.pricing * object.count * (100 + object.uplift.to_i) / 100
-    when :disbursement
+    end
+
+    def disbursement_cost(object)
       if object.disbursement_type.value == 'other'
         object.total_cost_without_vat
       else
