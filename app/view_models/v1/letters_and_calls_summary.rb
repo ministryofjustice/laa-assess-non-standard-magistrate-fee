445f09ee41b0902fd8e05a663e3e0b6b8d5ba343
David Henry <david.henry@digital.justice.gov.uk>
2023-09-21 17:04:00 +0100

    Align all the pages

--- app/view_models/v1/letters_and_calls_summary.rb
+++ app/view_models/v1/letters_and_calls_summary.rb
@@ -2,22 +2,6 @@ module V1
   class LettersAndCallsSummary < BaseViewModel
     attribute :letters_and_calls
 
-    def table_fields
-      data_by_type.map do |letter_and_call_type, total_cost|
-        [
-          letter_and_call_type,
-          NumberTo.pounds(total_cost)
-        ]
-      end
-    end
-
-    def summed_fields
-      total_cost = data_by_type.sum { |_, cost| cost }
-      [
-        NumberTo.pounds(total_cost)
-      ]
-    end
-
     def summary_row
       total_number_of_letters_and_calls = letters_and_calls.sum { |item| item['count'] }
       total_cost = data_by_type.sum { |_, cost| cost }
@@ -33,14 +17,10 @@ module V1
 
     private
 
-    def letters_or_calls
-      @letters_or_calls ||=
-        letters_and_calls.map { |letter_or_call| LetterAndCall.build_self(letter_or_call) }
-    end
-
     def data_by_type
       @data_by_type ||=
-        letters_or_calls.map do |letter_or_call|
+        letters_and_calls.map do |hash|
+          letter_or_call = LetterAndCall.build_self(hash)
           [
             letter_or_call.type.to_s,
             letter_or_call.provider_requested_amount
--- spec/view_models/v1/letters_and_calls_summary_spec.rb
+++ spec/view_models/v1/letters_and_calls_summary_spec.rb
@@ -10,18 +10,6 @@ RSpec.describe V1::LettersAndCallsSummary, type: :model do
   end
   let(:letters_and_calls_summary) { described_class.new(letters_and_calls:) }
 
-  describe '#table_fields' do
-    it 'returns an array of table fields' do
-      expect(letters_and_calls_summary.table_fields).to eq([['Letters', '£10.50']])
-    end
-  end
-
-  describe '#summed_fields' do
-    it 'returns an array of summed fields' do
-      expect(letters_and_calls_summary.summed_fields).to eq(['£10.50'])
-    end
-  end
-
   describe '#summary_row' do
     it 'returns an array of summary row fields' do
       expect(letters_and_calls_summary.summary_row).to eq(['12', '-', '£10.50', '-', '#pending#'])
