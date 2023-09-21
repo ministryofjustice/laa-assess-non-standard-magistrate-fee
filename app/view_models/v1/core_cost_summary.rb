3163c8b1d369ab53e639f7a0b6c33e7b791ede9b
David Henry <david.henry@digital.justice.gov.uk>
2023-09-21 13:30:02 +0100

    Start working on core costs

--- app/view_models/v1/core_cost_summary.rb
+++ app/view_models/v1/core_cost_summary.rb
@@ -1,13 +1,16 @@
 module V1
-  class WorkItemsSummary < BaseViewModel
-    attribute :work_items
+  class CoreCostSummary < BaseViewModel
+    SKIPPED_TYPES = %w[travel waiting].freeze
+]
+
+    attribute :work_items, :letters_annd_calls
 
     def table_fields
       data_by_type.map do |work_type, total_time_spent, total_cost|
         [
           work_type,
+          NumberTo.pounds(total_cost),
           "#{total_time_spent}min",
-          NumberTo.pounds(total_cost)
         ]
       end
     end
@@ -16,8 +19,8 @@ module V1
       total_time_spent = data_by_type.sum { |_, time_spent, _| time_spent }
       total_cost = data_by_type.sum { |_, _, cost| cost }
       [
+        NumberTo.pounds(total_cost),
         "#{total_time_spent}min",
-        NumberTo.pounds(total_cost)
       ]
     end
 
@@ -27,14 +30,15 @@ module V1
       @data_by_type ||=
         work_items.map { |work_item| WorkItem.build_self(work_item) }
                   .group_by { |work_item| work_item.work_type.to_s }
-                  .map do |work_type, work_items_for_type|
+                  .filter_map do |work_type, work_items_for_type|
+                    next if SKIPPED_TYPES.include?(work_type)
                     # TODO: convert this to a time period to enable easy formating of output
                     total_time_spent = work_items_for_type.sum(&:time_spent)
                     total_cost = work_items_for_type.sum { |work_item| CostCalculator.cost(:work_item, work_item) }
                     [
                       work_type,
+                      total_cost,
                       total_time_spent,
-                      total_cost
                     ]
                   end
     end
