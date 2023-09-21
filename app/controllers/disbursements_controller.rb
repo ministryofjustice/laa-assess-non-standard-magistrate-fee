6652a20c64db82a6acc0229cdb2d8aeb99230688
David Henry <david.henry@digital.justice.gov.uk>
2023-09-21 15:19:35 +0100

    add core costs to all screens

--- app/controllers/disbursements_controller.rb
+++ app/controllers/disbursements_controller.rb
@@ -4,6 +4,7 @@ class DisbursementsController < ApplicationController
   def index
     claim = Claim.find(params[:claim_id])
     disbursements = BaseViewModel.build_all(:disbursement, claim, 'disbursements').group_by(&:disbursement_date)
+    core_cost_summary = BaseViewModel.build(:core_cost_sumamry, claim)
 
     render locals: { claim:, disbursements: }
   end
--- app/controllers/work_items_controller.rb
+++ app/controllers/work_items_controller.rb
@@ -4,7 +4,7 @@ class WorkItemsController < ApplicationController
   def index
     claim = Claim.find(params[:claim_id])
     work_items = BaseViewModel.build_all(:work_item, claim, 'work_items').group_by(&:completed_on)
-    work_items_summary = BaseViewModel.build(:work_items_summary, claim)
+    core_cost_summary = BaseViewModel.build(:core_cost_sumamry, claim)
 
     render locals: { claim:, work_items:, work_items_summary: }
   end
