943e9c2d512c8186af99df290a4569f1ede930b4
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2023-09-22 10:09:33 +0100

    Adds some code cleanup

--- app/controllers/supporting_evidences_controller.rb
+++ app/controllers/supporting_evidences_controller.rb
@@ -2,15 +2,12 @@ require 'pagy/extras/array'
 class SupportingEvidencesController < ApplicationController
   include Pagy::Backend
 
-  DEFAULT_PAGE_SIZE = 5
   def show
     claim = Claim.find(params[:claim_id])
     claim_summary = BaseViewModel.build(:claim_summary, claim)
     supporting_evidence = BaseViewModel.build_all(:supporting_evidence, claim, 'supporting_evidences')
-    @pagy, @supporting_evidence = pagy_array(
-      supporting_evidence,
-      items: params.fetch(:page_size, DEFAULT_PAGE_SIZE)
-    )
-    render locals: { claim:, claim_summary:, supporting_evidence: }
+
+    @pagy, @supporting_evidence = pagy_array(supporting_evidence)
+    render locals: { claim:, claim_summary: }
   end
 end
