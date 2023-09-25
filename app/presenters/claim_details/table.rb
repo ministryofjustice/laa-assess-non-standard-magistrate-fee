4b27a5811d191acf51bf516fb8bc4258e88c30a2
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-09-25 14:34:15 +0100

    add claim justification

--- app/presenters/claim_details/table.rb
+++ app/presenters/claim_details/table.rb
@@ -1,12 +1,13 @@
 module ClaimDetails
   class Table
-    attr_reader :details_of_claim, :defendant_details, :case_details, :case_disposal
+    attr_reader :details_of_claim, :defendant_details, :case_details, :case_disposal, :claim_justification
 
     def initialize(claim)
       @details_of_claim = BaseViewModel.build(:details_of_claim, claim)
       @defendant_details = BaseViewModel.build(:defendant_details, claim)
       @case_details = BaseViewModel.build(:case_details, claim)
       @case_disposal = BaseViewModel.build(:case_disposal, claim)
+      @claim_justification = BaseViewModel.build(:claim_justification, claim)
     end
 
     # rubocop:disable Metrics/AbcSize
@@ -15,7 +16,8 @@ module ClaimDetails
         details_of_claim.rows,
         defendant_details.rows,
         case_details.rows,
-        case_disposal.rows
+        case_disposal.rows,
+        claim_justification.rows
       ]
     end
     # rubocop:enable Metrics/AbcSize
---
+++ app/view_models/v1/claim_justification.rb
@@ -0,0 +1,31 @@
+module V1
+  class ClaimJustification < BaseViewModel
+    attribute :reasons_for_claim
+
+    def key
+      'claim_justification'
+    end
+
+    def title
+      I18n.t(".claim_details.#{key}.title")
+    end
+
+    def reasons_for_claim_list
+      reasons = reasons_for_claim.pluck('en')
+      ApplicationController.helpers.sanitize(reasons.join('<br>'), tags: %w[br])
+    end
+
+    def data
+      [
+        {
+          title: I18n.t('.claim_details.claim_justification.reasons_for_claim'),
+          value: reasons_for_claim_list
+        }
+      ]
+    end
+
+    def rows
+      { title:, data: }
+    end
+  end
+end
