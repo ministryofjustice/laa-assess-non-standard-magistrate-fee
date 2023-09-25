8dbda16f605e66f9636deb1a2f122cbf7422ef41
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-09-25 17:07:31 +0100

    add case disposal tests

---
+++ spec/view_models/v1/case_disposal_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe V1::CaseDisposal do
+  describe '#title' do
+    it 'shows correct title' do
+      expect(subject.title).to eq('Case disposal')
+    end
+  end
+
+  describe '#data' do
+    subject = described_class.new(
+      {
+        plea: {
+          'value': 'cracked_trial',
+          'en': 'Cracked Trial'
+        },
+        plea_category: 'Category 2'
+      }
+    )
+
+    it 'shows correct table data' do
+      expect(subject.data).to eq(
+        [
+          { title: 'Category 2', value: 'Cracked Trial' }
+        ]
+      )
+    end
+  end
+end
