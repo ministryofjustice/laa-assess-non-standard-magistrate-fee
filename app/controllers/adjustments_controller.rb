0e015136463fc9082975d5a1ad0935c4d1d30bda
David Henry <david.henry@digital.justice.gov.uk>
2023-09-25 15:15:25 +0100

    Crm457 488/core cost summary (#47)
    ## Description of change
    Replace WorkItemSummary with CoreCostSummary
    
    ## Link to relevant ticket
    
    [CRM457-488](https://dsdmoj.atlassian.net/browse/CRM457-488)
    
    ## Screenshots of changes (if applicable)
    <img width="718" alt="Screenshot 2023-09-21 at 17 04 09"
    src="https://github.com/ministryofjustice/laa-assess-non-standard-magistrate-fee/assets/63736/37625acb-23ac-49f1-ba8e-c6dff47d9c76">

--- app/controllers/adjustments_controller.rb
+++ app/controllers/adjustments_controller.rb
@@ -2,7 +2,8 @@ class AdjustmentsController < ApplicationController
   def show
     claim = Claim.find(params[:claim_id])
     claim_summary = BaseViewModel.build(:claim_summary, claim)
+    core_cost_summary = BaseViewModel.build(:core_cost_summary, claim)
 
-    render locals: { claim:, claim_summary: }
+    render locals: { claim:, claim_summary:, core_cost_summary: }
   end
 end
--- spec/controllers/work_items_controller_spec.rb
+++ spec/controllers/work_items_controller_spec.rb
@@ -6,11 +6,9 @@ RSpec.describe WorkItemsController do
     let(:claim_id) { SecureRandom.uuid }
     let(:work_items) { [instance_double(V1::WorkItem, completed_on: Time.zone.today)] }
     let(:grouped_work_items) { { Time.zone.today => work_items } }
-    let(:work_items_summary) { instance_double(V1::WorkItemsSummary) }
 
     before do
       allow(Claim).to receive(:find).and_return(claim)
-      allow(BaseViewModel).to receive_messages(build: work_items_summary)
       allow(BaseViewModel).to receive_messages(build_all: work_items)
     end
 
@@ -19,15 +17,13 @@ RSpec.describe WorkItemsController do
 
       expect(Claim).to have_received(:find).with(claim_id)
       expect(BaseViewModel).to have_received(:build_all).with(:work_item, claim, 'work_items')
-      expect(BaseViewModel).to have_received(:build).with(:work_items_summary, claim)
     end
 
     it 'renders successfully with claims' do
       allow(controller).to receive(:render)
       get :index, params: { claim_id: }
 
-      expect(controller).to have_received(:render).with(locals: { claim: claim, work_items: grouped_work_items,
-work_items_summary: work_items_summary })
+      expect(controller).to have_received(:render).with(locals: { claim: claim, work_items: grouped_work_items })
       expect(response).to be_successful
     end
   end
