0a5cbb1fcb3cc2638b5b0f4728177b7896791805
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-09-26 09:56:20 +0100

    fix case details spec and create claim details spec

--- spec/view_models/v1/case_details_spec.rb
+++ spec/view_models/v1/case_details_spec.rb
@@ -8,31 +8,45 @@ RSpec.describe V1::CaseDetails do
   end
 
   describe '#data' do
-    subject = described_class.new(
-      {
-        main_offence: 'Stole an apple',
-        main_offence_date: '2023-01-01',
-        assigned_counsel: 'yes',
-        unassigned_counsel: 'no',
-        agent_instructed: 'yes',
-        remitted_to_magistrate: 'yes',
-        remitted_to_magistrate_date: '2023-02-01'
-      }
-    )
+    context 'Remittal' do
+      subject = described_class.new(
+        {
+          main_offence: 'Stole an apple',
+          main_offence_date: '2023-01-01',
+          assigned_counsel: 'yes',
+          unassigned_counsel: 'no',
+          agent_instructed: 'yes',
+          remitted_to_magistrate: 'yes',
+          remitted_to_magistrate_date: '2023-02-01'
+        }
+      )
 
-    it 'shows correct table data' do
-      expect(subject.data).to eq([
-                                   { title: 'Main offence name', value: 'Stole an apple' },
-                                   { title: 'Main offence date', value: '01 January 2023' },
-                                   { title: 'Assigned counsel', value: 'Yes' },
-                                   { title: 'Unassigned counsel', value: 'No' },
-                                   { title: 'Instructed agent', value: 'Yes' },
-                                   { title: "Case remitted from Crown Court to magistrates' court", value: 'Yes' },
-                                   { title: 'Remittal date', value: '01 February 2023' },
-                                 ])
+      it 'shows correct table data' do
+        expect(subject.data).to eq([
+                                    { title: 'Main offence name', value: 'Stole an apple' },
+                                    { title: 'Main offence date', value: '01 January 2023' },
+                                    { title: 'Assigned counsel', value: 'Yes' },
+                                    { title: 'Unassigned counsel', value: 'No' },
+                                    { title: 'Instructed agent', value: 'Yes' },
+                                    { title: "Case remitted from Crown Court to magistrates' court", value: 'Yes' },
+                                    { title: 'Remittal date', value: '01 February 2023' },
+                                  ])
+      end
     end
 
     context 'No remittal' do
+      subject = described_class.new(
+        {
+          main_offence: 'Stole an apple',
+          main_offence_date: '2023-01-01',
+          assigned_counsel: 'yes',
+          unassigned_counsel: 'no',
+          agent_instructed: 'yes',
+          remitted_to_magistrate: 'no',
+          remitted_to_magistrate_date: '2023-02-01'
+        }
+      )
+
       it 'shows correct table data' do
         expect(subject.data).to eq([
                                      { title: 'Main offence name', value: 'Stole an apple' },
