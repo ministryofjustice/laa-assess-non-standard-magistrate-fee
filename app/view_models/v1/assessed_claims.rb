d53c26acbe1ec2e7b9ca07767ec28e03fc581754
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2023-10-11 10:02:31 +0100

    Adds table view in the page

---
+++ app/view_models/v1/assessed_claims.rb
@@ -0,0 +1,38 @@
+module V1
+  class AssessedClaims < BaseViewModel
+    attribute :laa_reference
+    attribute :defendants
+    attribute :firm_office
+    attribute :updated_at, :date
+    attribute :id
+    attribute :state
+
+    def main_defendant_name
+      main_defendant = defendants.detect { |defendant| defendant['main'] }
+      main_defendant ? main_defendant['full_name'] : ''
+    end
+
+    def firm_name
+      firm_office['name']
+    end
+
+    def date_assessed
+      { text: I18n.l(updated_at, format: '%-d %b %Y'), sort_value: updated_at.to_fs(:db) }
+    end
+
+    def case_worker_name
+      '#Pending#'
+    end
+
+    def table_fields
+      [
+        { laa_reference: laa_reference, claim_id: id },
+        firm_name,
+        main_defendant_name,
+        date_assessed,
+        case_worker_name,
+        state
+      ]
+    end
+  end
+end
---
+++ spec/view_models/v1/assessed_claims_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.describe V1::AssessedClaims, type: :view_model do
+  subject(:all_claims) do
+    described_class.new(laa_reference:, defendants:, firm_office:, updated_at:, id:, state:)
+  end
+
+  let(:laa_reference) { '1234567890' }
+  let(:defendants) { [{ 'full_name' => 'John Doe', 'main' => true }, 'main' => false, 'full_name' => 'jimbob'] }
+  let(:firm_office) { { 'name' => 'Acme Law Firm' } }
+  let(:updated_at) { Time.zone.yesterday }
+  let(:id) { 1 }
+  let(:state) { 'grant' }
+
+  describe '#main_defendant_name' do
+    it 'returns the name of the main defendant' do
+      summary = described_class.new('defendants' => defendants)
+      expect(summary.main_defendant_name).to eq('John Doe')
+    end
+
+    context 'when no main defendant record - shouold not be possible' do
+      it 'returns an empty string' do
+        defendants = [
+          { 'main' => false, 'full_name' => 'John Doe' },
+        ]
+        summary = described_class.new('defendants' => defendants)
+        expect(summary.main_defendant_name).to eq('')
+      end
+    end
+  end
+
+  describe '#firm_name' do
+    it 'returns the name of the firm office' do
+      expect(all_claims.firm_name).to eq('Acme Law Firm')
+    end
+  end
+
+  describe '#case_worker_name' do
+    it 'returns the pending status' do
+      expect(all_claims.case_worker_name).to eq('#Pending#')
+    end
+  end
+
+  describe '#table_fields' do
+    it 'returns an array of table fields' do
+      expected_fields = [
+        { laa_reference: '1234567890', claim_id: 1 },
+        'Acme Law Firm',
+        'John Doe',
+        { text: I18n.l(Time.zone.yesterday, format: '%-d %b %Y'), sort_value: Time.zone.yesterday.to_fs(:db) },
+        '#Pending#',
+        'grant'
+      ]
+      expect(all_claims.table_fields).to eq(expected_fields)
+    end
+  end
+end
