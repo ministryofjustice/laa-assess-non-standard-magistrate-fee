413967e6209de5c9c39679c70cc19a8b4575703e
David Henry <david.henry@digital.justice.gov.uk>
2023-10-13 10:17:55 +0100

    [CRM457-511] Set update message when decision is made (#73)
    ## Description of change
    set update message after decision
    
    - fix redirect
    - fix some broke column classes
    - fix turbo related issue with make decision screen
    
    ## Link to relevant ticket
    [CRM457-511](https://dsdmoj.atlassian.net/browse/CRM457-511)
    
    ## Screenshots of changes (if applicable)
    <img width="820" alt="Screenshot 2023-10-12 at 15 44 54"
    src="https://github.com/ministryofjustice/laa-assess-non-standard-magistrate-fee/assets/63736/07924251-5385-4252-9081-9a7d0bb883c1">
    
    
    [CRM457-511]:
    https://dsdmoj.atlassian.net/browse/CRM457-511?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- app/controllers/make_decisions_controller.rb
+++ app/controllers/make_decisions_controller.rb
@@ -1,22 +1,30 @@
 class MakeDecisionsController < ApplicationController
   def edit
-    claim = Claim.find(params[:claim_id])
     decision = MakeDecisionForm.new(id: params[:claim_id])
     render locals: { claim:, decision: }
   end
 
   # TODO: put some sort of permissions here for non supervisors?
   def update
-    claim = Claim.find(params[:claim_id])
     decision = MakeDecisionForm.new(decision_params)
     if decision.save
-      redirect_to claims_path, flash: { success: 'claim success text' }
+      reference = BaseViewModel.build(:laa_reference, claim)
+      success_notice = t(
+        ".decision.#{decision.state}",
+        ref: reference.laa_reference,
+        url: claim_claim_details_path(claim.id)
+      )
+      redirect_to assessed_claims_path, flash: { success: success_notice }
     else
       render :edit, locals: { claim:, decision: }
     end
   end
 
   private
+
+  def claim
+    @claim ||= Claim.find(params[:claim_id])
+  end
 
   # TODO: user current_user once merged
   def decision_params
