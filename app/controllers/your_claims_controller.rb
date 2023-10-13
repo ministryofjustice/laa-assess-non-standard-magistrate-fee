2460ccb344ffe14ddc86138aef57419dc38325d0
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2023-10-13 10:32:15 +0100

    Added todo to address future work

--- app/controllers/your_claims_controller.rb
+++ app/controllers/your_claims_controller.rb
@@ -5,6 +5,7 @@ class YourClaimsController < ApplicationController
     claims = Claim.your_claims
     claims = claims.map { |claim| BaseViewModel.build(:your_claims, claim) }
     @pagy, @claims = pagy_array(claims)
+    # TODO: Next claim assignment needs to be done
     @next_claim = Claim.unassigned_claims.order(created_at: :desc).first
   end
 end
