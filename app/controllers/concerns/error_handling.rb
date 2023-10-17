2222fb1dded61882635a45f9252d1f7bcf922a45
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-10-17 14:38:57 +0100

    add redirect to error page upon error

--- app/controllers/concerns/error_handling.rb
+++ app/controllers/concerns/error_handling.rb
@@ -7,6 +7,16 @@ module ErrorHandling
 
       Sentry.capture_exception(exception) if ENV.fetch('SENTRY_DSN', nil).present?
       Rails.logger.error(exception)
+      respond_with_status(:internal_server_error)
+    end
+  end
+
+  private
+
+  def respond_with_status(status)
+    respond_to do |format|
+      format.html { render status: }
+      format.all  { head status }
     end
   end
 end
