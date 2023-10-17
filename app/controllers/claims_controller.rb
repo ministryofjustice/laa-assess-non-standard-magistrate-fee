8cf43617a00f038820652c623a1801a29dc3dbd4
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-10-17 16:25:36 +0100

    force failure for testing

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -2,6 +2,7 @@ require 'pagy/extras/array'
 class ClaimsController < ApplicationController
   include Pagy::Backend
   def index
+    1 / 0
     claims = Claim.pending_decision
     claims = claims.map { |claim| BaseViewModel.build(:all_claims, claim) }
     @pagy, @claims = pagy_array(claims)
