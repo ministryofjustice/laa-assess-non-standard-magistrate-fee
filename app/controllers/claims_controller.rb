ef0749a37be63899b5808b1ae54710f9576c412b
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-10-18 15:37:23 +0100

    Code cleanup
    Ignoring test for now because need more time to figure out best way to test concern

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -2,7 +2,6 @@ require 'pagy/extras/array'
 class ClaimsController < ApplicationController
   include Pagy::Backend
   def index
-    1 / 0
     claims = Claim.pending_decision
     claims = claims.map { |claim| BaseViewModel.build(:all_claims, claim) }
     @pagy, @claims = pagy_array(claims)
