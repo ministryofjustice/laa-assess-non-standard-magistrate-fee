612f9db6dd63556867213d6c3cf1de1a8b535b9d
David Henry <david.henry@digital.justice.gov.uk>
2023-10-23 14:00:35 +0100

    extract constants to improve readibility

--- app/forms/letters_calls_form.rb
+++ app/forms/letters_calls_form.rb
@@ -2,11 +2,13 @@ class LettersCallsForm
   include ActiveModel::Model
   include ActiveModel::Attributes
   include ActiveRecord::AttributeAssignment
+  UPLIFT_PROVIDED = 'no'
+  UPLIFT_RESET = 'yes'
 
   attribute :id
   attribute :type, :string
   attribute :uplift, :string
-  # not set to integer so we can catch errors is non-number values are entered
+  # not set to integer so we can catch errors if non-number values are entered
   attribute :count
   attribute :explanation, :string
   attribute :current_user
@@ -14,7 +16,7 @@ class LettersCallsForm
 
   validates :claim, presence: true
   validates :type, inclusion: { in: %w[letters calls] }
-  validates :uplift, inclusion: { in: %w[yes no] }, if: -> { item.uplift? }
+  validates :uplift, inclusion: { in: [UPLIFT_PROVIDED, UPLIFT_RESET] }, if: -> { item.uplift? }
   validates :count, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :explanation, presence: true, if: :data_has_changed?
   validate :data_changed
@@ -86,10 +88,9 @@ class LettersCallsForm
     errors.add(:base, :no_change)
   end
 
-  # we need the item object to
   def data_has_changed?
-    # change to count || uplift and uplift has changed
-    count.to_i != item.count || (item.uplift? && item.uplift.zero? == (uplift == 'no'))
+    count.to_i != item.count ||
+      (item.uplift? && item.uplift.zero? != (uplift == UPLIFT_RESET))
   end
 
   def version
