a2de95e3c21181c45697ecc4955ce67627ba26e4
Imtiaz Ahmed <90189839+imtiazAhmd@users.noreply.github.com>
2023-10-26 12:43:26 +0100

    Added button and table for calculation (#94)
    ## Description of change
    Added button and table for calculation after adjustment
    
    ## Link to relevant ticket
    
    [](https://dsdmoj.atlassian.net/browse/)
    
    
    ## Screenshots of changes 
    ![Screenshot 2023-10-26 at 12 04
    28](https://github.com/ministryofjustice/laa-assess-non-standard-magistrate-fee/assets/90189839/90ada199-8ad8-405f-a504-a08c8be63016)
    
    
    ## How to manually test the feature

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,7 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "./component/sortable_table"
+import "./component/letters_calls_adjustment"
 // https://frontend.design-system.service.gov.uk/importing-css-assets-and-javascript/#javascript
 import { initAll } from 'govuk-frontend'
 
