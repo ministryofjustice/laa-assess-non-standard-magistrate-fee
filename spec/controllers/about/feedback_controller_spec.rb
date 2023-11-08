83d07ad3ae4e0bb00d2436f4283bb77406328612
Paul King <84575729+pking-moj@users.noreply.github.com>
2023-11-08 17:09:37 +0000

    Add feedback form

---
+++ spec/controllers/about/feedback_controller_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe About::FeedbackController do
+  let(:message_delivery) { instance_double(FeedbackMailer::MessageDelivery) }
+
+  describe '#feedback' do
+    context 'index' do
+      it 'has a 200 response code' do
+        get :index
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'submitting' do
+      before do
+        allow(FeedbackMailer).to receive(:notify).and_return(message_delivery)
+        allow(message_delivery).to receive(:deliver_later!)
+        post :create, params: feedback_params
+      end
+
+      context 'valid feedback' do
+        let(:feedback_params) do
+          {
+            feedback: {
+              user_email: 'test@test.com',
+              user_feedback: 'test',
+              user_rating: 5
+            }
+          }
+        end
+
+        it 'redirects to root' do
+          expect(response).to redirect_to(claims_path)
+        end
+
+        it 'flashes notice' do
+          expect(flash.now[:notice]).to match(/Your feedback has been submitted/)
+        end
+      end
+
+      context 'invalid feedback' do
+        let(:feedback_params) do
+          {
+            feedback: {
+              user_email: 'test@test.com',
+              user_feedback: 'test'
+            }
+          }
+        end
+
+        it 'returns an error' do
+          expect(response).not_to redirect_to(claims_path)
+          expect(response).to match(/Help us improve this service/)
+        end
+      end
+    end
+  end
+end
