ed65b0d10a1261420c95e3a49bf633d58c7c026e
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-11-14 14:25:16 +0000

    fix/add tests for work item and travel and waiting

--- spec/view_models/v1/work_item_spec.rb
+++ spec/view_models/v1/work_item_spec.rb
@@ -1,25 +1,98 @@
 require 'rails_helper'
 
 RSpec.describe V1::WorkItem do
-  describe 'adjustment' do
-    it 'returns pending' do
-      subject = described_class.new
-      expect(subject.adjustments).to eq('#pending#')
-    end
-  end
+  subject { described_class.new(params) }
+
+  let(:adjustments) { [] }
 
   describe 'table_fields' do
+    let(:params) do
+      {
+        'work_type' => { 'en' => 'Waiting', 'value' => 'waiting' },
+        'time_spent' => 161,
+        'uplift' => 0,
+        'pricing' => 24.0,
+        'adjustments' => adjustments
+      }
+    end
+
+    before do
+      allow(CostCalculator).to receive(:cost).and_return(10.0)
+    end
+
     it 'returns the fields for the table display' do
-      subject = described_class.new('work_type' => { 'en' => 'waiting' }, 'time_spent' => 61)
-      expect(subject.table_fields).to eq(['waiting', '0%', '61min', '#pending#', '#pending#'])
+      expect(subject.table_fields).to eq(['Waiting', '0%', '£10.00', '', ''])
+    end
+
+    context 'when adjustments exists' do
+      let(:adjustments) { [build(:event, :edit_work_item_uplift)] }
+
+      it 'also renders caseworker values' do
+        expect(subject.table_fields).to eq(['Waiting', '20%', '£10.00', '0%', '£10.00'])
+      end
     end
   end
 
   describe 'provider_requested_amount' do
-    subject = described_class.new({ pricing: 20, time_spent: 90, uplift: 15 })
+    let(:params) { {
+      'time_spent' => 171,
+      'uplift' => 10,
+      'pricing' => 24.0
+    }}
 
     it 'calculates the correct provider requested amount' do
-      expect(subject.provider_requested_amount).to eq(34.5)
+      expect(subject.provider_requested_amount).to eq(75.24)
+    end
+  end
+
+  describe 'provider_requested_time_spent' do
+    let(:params) {
+      {
+        'time_spent' => 100,
+        'uplift' => 0,
+        'pricing' => 24.0,
+        'adjustments' => adjustments
+      }
+    }
+
+    let(:adjustments) { [build(:event, :edit_work_item_time_spent)] }
+
+    it 'shows the correct provider requested time spent' do
+      expect(subject.provider_requested_time_spent).to eq(171)
+    end
+  end
+
+  describe 'caseworker_amount' do
+    let(:params) {
+      {
+        'time_spent' => 171,
+        'uplift' => 0,
+        'pricing' => 24.0,
+        'adjustments' => adjustments
+      }
+    }
+
+    let(:adjustments) { [build(:event, :edit_work_item_uplift)] }
+
+    it 'calculates the correct caseworker requested amount' do
+      expect(subject.caseworker_amount).to eq(68.4)
+    end
+  end
+
+  describe 'provider_requested_uplift' do
+    let(:params) {
+      {
+        'time_spent' => 171,
+        'uplift' => 0,
+        'pricing' => 24.0,
+        'adjustments' => adjustments
+      }
+    }
+
+    let(:adjustments) { [build(:event, :edit_work_item_uplift)] }
+
+    it 'shows the correct provider requested uplift' do
+      expect(subject.provider_requested_uplift).to eq(20)
     end
   end
 end
