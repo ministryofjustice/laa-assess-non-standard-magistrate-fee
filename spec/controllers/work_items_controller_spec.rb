d12423838ff38d0258d9a3f75fd02032d12d1b9c
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-11-14 17:53:56 +0000

    add tests for work item controller

--- spec/controllers/work_items_controller_spec.rb
+++ spec/controllers/work_items_controller_spec.rb
@@ -36,8 +36,8 @@ RSpec.describe WorkItemsController do
   context 'edit' do
     let(:claim) { instance_double(Claim, id: claim_id) }
     let(:claim_id) { SecureRandom.uuid }
-    let(:waiting_id) { SecureRandom.uuid }
     let(:travel_id) { SecureRandom.uuid }
+    let(:waiting_id) { SecureRandom.uuid }
     let(:waiting) do
       instance_double(V1::WorkItem, id: waiting_id, work_type: double(value: 'waiting'), form_attributes: {})
     end
@@ -53,26 +53,57 @@ RSpec.describe WorkItemsController do
       allow(WorkItemForm).to receive(:new).and_return(form)
     end
 
-    context 'when URL is for Waiting' do
+    it 'renders sucessfully with claims' do
+      allow(controller).to receive(:render)
+      get :edit, params: { claim_id: claim_id, id: waiting_id, form: form }
+
+      expect(controller).to have_received(:render)
+                        .with(locals: { claim: claim, item: waiting, form: form })
+      expect(response).to be_successful
+    end
+  end
+
+  context 'update' do
+    let(:claim) { instance_double(Claim, id: claim_id) }
+    let(:claim_id) { SecureRandom.uuid }
+    let(:waiting_id) { SecureRandom.uuid }
+    let(:travel_id) { SecureRandom.uuid }
+    let(:waiting) do
+      instance_double(V1::WorkItem, id: waiting_id, work_type: double(value: 'waiting'), form_attributes: {})
+    end
+    let(:travel) do
+      instance_double(V1::WorkItem, id: travel_id, work_type: double(value: 'travel'), form_attributes: {})
+    end
+    let(:work_items) { [waiting, travel] }
+    let(:form) { instance_double(WorkItemForm, save:) }
+
+    before do
+      allow(Claim).to receive(:find).and_return(claim)
+      allow(BaseViewModel).to receive(:build).and_return(work_items)
+      allow(WorkItemForm).to receive(:new).and_return(form)
+    end
+
+    context 'when form save is successful' do
+      let(:save) { true }
+
       it 'renders sucessfully with claims' do
         allow(controller).to receive(:render)
-        get :edit, params: { claim_id: claim_id, id: waiting_id, form: form }
+        get :update, params: { claim_id: claim_id, id: waiting_id, work_item_form: { some: :data } }
 
-        expect(controller).to have_received(:render)
-                          .with(locals: { claim: claim, item: waiting, form: form })
-        expect(response).to be_successful
+        expect(controller).to redirect_to(claim_adjustments_path(claim, anchor: 'work-items-tab'))
+        expect(response).to have_http_status(:found)
       end
     end
 
-    context 'when URL is for Travel' do
-      let(:id) { 'travel' }
+    context 'when form save is unsuccessful' do
+      let(:save) { false }
 
-      it 'renders sucessfully with claims' do
+      it 'renders successfully with claims' do
         allow(controller).to receive(:render)
-        get :edit, params: { claim_id: claim_id, id: travel_id, form: form }
+        put :update, params: { claim_id: claim_id, id: waiting_id, work_item_form: { some: :data } }
 
         expect(controller).to have_received(:render)
-                          .with(locals: { claim: claim, item: travel, form: form })
+                          .with(:edit, locals: { claim: claim, form: form, item: waiting })
         expect(response).to be_successful
       end
     end
