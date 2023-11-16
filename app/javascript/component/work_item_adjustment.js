74d327deba4da98725970b9cb54b9822e83fd596
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-11-16 14:03:08 +0000

    add js code to show -- if non number entry put in time spent

--- app/javascript/component/work_item_adjustment.js
+++ app/javascript/component/work_item_adjustment.js
@@ -33,6 +33,10 @@ function init() {
 
     var minutes = calculateChangeButton?.getAttribute('data-provider-time-spent');
 
+    if(isNaN(Number(hoursField?.value)) || isNaN(Number(minutesField?.value))){
+      return '--';
+    }
+
     if(hoursField?.value && minutesField?.value){
       minutes = (parseInt(hoursField.value) * 60) + parseInt(minutesField.value);
     }
@@ -70,7 +74,7 @@ function init() {
   }
 
   function getProviderUplift(){
-    return calculateChangeButton?.getAttribute('data-uplift-amount')
+    return calculateChangeButton?.getAttribute('data-uplift-amount');
   }
 }
 
