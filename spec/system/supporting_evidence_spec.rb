80705025c606fad5e05862c6538c97e8772dac7e
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2023-11-21 15:25:48 +0000

    Added tests for when supporting evidence exists

--- spec/system/supporting_evidence_spec.rb
+++ spec/system/supporting_evidence_spec.rb
@@ -6,12 +6,28 @@ RSpec.describe 'Supporting Evidence' do
 
   before do
     sign_in user
+    visit claim_supporting_evidences_path(claim)
   end
 
   context 'There is supporting evidence and nothing is sent by post' do
     it 'can view supporting evidence table' do
-      visit claim_supporting_evidences_path(claim)
-      save_and_open_page
+      within('.govuk-table__row', text: 'Advocacy evidence _ Tom_TC.pdf') do
+        expect(page).to have_content(
+          'Advocacy evidence _ Tom_TC.pdf' \
+          'Monday18 September 2023'
+        )
+      end
+    end
+
+    it 'no send by post info shown' do
+      expect(page).to have_no_content('The provider has chosen to post the evidence to:')
+    end
+  end
+
+  context 'There is supporting evidence and some evidence is sent by post' do
+    let(:claim) { create(:claim, :sent_by_post_true_with_evidence) }
+
+    it 'can view supporting evidence table' do
 
       within('.govuk-table__row', text: 'Advocacy evidence _ Tom_TC.pdf') do
         expect(page).to have_content(
@@ -20,11 +36,11 @@ RSpec.describe 'Supporting Evidence' do
         )
       end
     end
+
+    it 'send by post info is shown' do
+      expect(page).to have_content('The provider has chosen to post the evidence to:')
+    end
   end
-
-  # context 'There is supporting evidence and some evidence is sent by post' do
-
-  # end
 
   # context 'There is supporting evidence sent by post' do
 
