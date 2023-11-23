0372837beb0b9906fb857ded5fc84fb996ab246c
Paul King <84575729+pking-moj@users.noreply.github.com>
2023-11-23 16:23:37 +0000

    Provide presigned URLs that expire after 15 mins

--- app/controllers/supporting_evidences_controller.rb
+++ app/controllers/supporting_evidences_controller.rb
@@ -4,7 +4,20 @@ class SupportingEvidencesController < ApplicationController
     claim_summary = BaseViewModel.build(:claim_summary, claim)
     supporting_evidence = BaseViewModel.build(:supporting_evidence, claim, 'supporting_evidences')
 
+    generate_urls supporting_evidence
+
     @pagy, @supporting_evidence = pagy_array(supporting_evidence)
     render locals: { claim:, claim_summary: }
+  end
+
+  private
+
+  def generate_urls(supporting_evidence)
+    supporting_evidence.each do | item |
+      item.download_url = S3_BUCKET
+                          .object(item.file_path)
+                          .presigned_url(:get, expires_in: 900,
+                                         response_content_disposition: "attachment; filename=#{item.file_name}")
+    end
   end
 end
