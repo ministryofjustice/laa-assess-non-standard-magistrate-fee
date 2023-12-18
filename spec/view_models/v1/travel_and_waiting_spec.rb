b572cfc595d41b2e1a3b484bb867b6d5c5182ace
David Henry <david.henry@digital.justice.gov.uk>
2023-12-18 11:07:29 +0000

    cleaner tests

--- spec/view_models/v1/travel_and_waiting_spec.rb
+++ spec/view_models/v1/travel_and_waiting_spec.rb
@@ -38,63 +38,40 @@ RSpec.describe V1::TravelAndWaiting do
 
   describe '#table_fields' do
     context 'when a single work item exists' do
-      let(:work_items) { [{ 'work_type' => { 'en' => 'travel', 'value' => 'travel' }, 'time_spent' => 20 }] }
-      let(:work_item) do
-        instance_double(V1::WorkItem, work_type: mock_translated('travel'), time_spent: 20,
-       provider_requested_time_spent: 20)
-      end
-
-      before do
-        allow(BaseViewModel).to receive(:build).and_return([work_item])
-      end
-
-      it 'builds the view model' do
-        subject.table_fields
-        expect(BaseViewModel).to have_received(:build).with(
-          :work_item, claim, 'work_items'
-        )
-      end
+      let(:work_items) { [{ 'work_type' => { 'en' => 'Travel', 'value' => 'travel' }, 'time_spent' => 20, 'vat_rate' => 0.2 }] }
 
       it 'includes the summed table field row' do
         expect(subject.table_fields).to include(
-          ['Travel', '0 Hours<br>20 Minutes', '£100.00', '0 Hours<br>20 Minutes', '£100.00'],
+          ['Travel', '0 Hours<br>20 Minutes', '£120.00', '0 Hours<br>20 Minutes', '£120.00'],
         )
       end
 
-      it 'calls the CostCalculator' do
-        subject.table_fields
-
-        expect(CostCalculator).to have_received(:cost).with(:work_item, work_item, :caseworker)
-      end
-
-      it { expect(subject).to be_any }
-
       it 'calculates the total_cost' do
-        expect(subject.total_cost).to eq('£100.00')
+        expect(subject.total_cost).to eq('£120.00')
       end
     end
 
     context 'when multiple work item of diffent types exists' do
       let(:work_items) do
-        [{ 'work_type' => { 'en' => 'Travel', 'value' => 'travel' }, 'time_spent' => 20 },
-         { 'work_type' => { 'en' => 'Waiting', 'value' => 'waiting' }, 'time_spent' => 30 }]
+        [{ 'work_type' => { 'en' => 'Travel', 'value' => 'travel' }, 'time_spent' => 20, 'vat_rate' => 0.2  },
+         { 'work_type' => { 'en' => 'Waiting', 'value' => 'waiting' }, 'time_spent' => 30, 'vat_rate' => 0.2 }]
       end
 
       it 'returns a single table field row' do
         expect(subject.table_fields).to include(
-          ['Travel', '0 Hours<br>20 Minutes', '£100.00', '0 Hours<br>20 Minutes', '£100.00'],
-          ['Waiting', '0 Hours<br>30 Minutes', '£100.00', '0 Hours<br>30 Minutes', '£100.00']
+          ['Travel', '0 Hours<br>20 Minutes', '£120.00', '0 Hours<br>20 Minutes', '£120.00'],
+          ['Waiting', '0 Hours<br>30 Minutes', '£120.00', '0 Hours<br>30 Minutes', '£120.00']
         )
       end
 
       it 'calculates the total_cost' do
-        expect(subject.total_cost).to eq('£200.00')
+        expect(subject.total_cost).to eq('£240.00')
       end
     end
 
     context 'when waiting and travel work items do not exist' do
       let(:work_items) do
-        [{ 'work_type' => { 'en' => 'preparation', 'value' => 'preparation' }, 'time_spent' => 30 }]
+        [{ 'work_type' => { 'en' => 'preparation', 'value' => 'preparation' }, 'time_spent' => 30, 'vat_rate' => 0.2  }]
       end
 
       it 'nothing is returned' do
@@ -106,13 +83,13 @@ RSpec.describe V1::TravelAndWaiting do
 
     context 'when multiple work item of the same types exists' do
       let(:work_items) do
-        [{ 'work_type' => { 'en' => 'Travel', 'value' => 'travel' }, 'time_spent' => 20 },
-         { 'work_type' => { 'en' => 'Travel', 'value' => 'travel' }, 'time_spent' => 30 }]
+        [{ 'work_type' => { 'en' => 'Travel', 'value' => 'travel' }, 'time_spent' => 20, 'vat_rate' => 0.2  },
+         { 'work_type' => { 'en' => 'Travel', 'value' => 'travel' }, 'time_spent' => 30, 'vat_rate' => 0.2  }]
       end
 
       it 'includes a summed table field row' do
         expect(subject.table_fields).to include(
-          ['Travel', '0 Hours<br>50 Minutes', '£200.00', '0 Hours<br>50 Minutes', '£200.00']
+          ['Travel', '0 Hours<br>50 Minutes', '£240.00', '0 Hours<br>50 Minutes', '£240.00']
         )
       end
     end
