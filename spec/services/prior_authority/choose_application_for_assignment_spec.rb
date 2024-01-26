5f764d574db49d544c895ff612f95b52be1dd452
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-01-26 14:50:01 +0000

    Restructure specs as per review comments

--- spec/services/prior_authority/choose_application_for_assignment_spec.rb
+++ spec/services/prior_authority/choose_application_for_assignment_spec.rb
@@ -3,30 +3,82 @@ require 'rails_helper'
 RSpec.describe PriorAuthority::ChooseApplicationForAssignment do
   let(:user) { create(:caseworker) }
 
-  it 'prefers central criminal court cases' do
-    central = create(:prior_authority_application,
-                     data: build(:prior_authority_data, court_type: 'central_criminal_court'))
-    create(:prior_authority_application, data: build(:prior_authority_data, court_type: 'other'))
-    expect(described_class.call(user)).to eq central
+  context 'when there is an older and a newer application' do
+    let(:newer_application) { create(:prior_authority_application, created_at: 1.day.ago) }
+    let(:older_application) { create(:prior_authority_application, created_at: 2.days.ago) }
+
+    before do
+      newer_application && older_application
+    end
+
+    it 'prefers earlier submisions' do
+      expect(described_class.call(user)).to eq older_application
+    end
   end
 
-  it 'prefers pathology court cases' do
-    create(:prior_authority_application, data: build(:prior_authority_data, service_type: 'other'))
-    pathology = create(:prior_authority_application, data: build(:prior_authority_data, service_type: 'pathologist'))
-    expect(described_class.call(user)).to eq pathology
+  context 'when the newer application is a central criminal court case' do
+    let(:central_court_application) do
+      create(:prior_authority_application,
+             created_at: 1.day.ago,
+                    data: build(:prior_authority_data, court_type: 'central_criminal_court'))
+    end
+
+    let(:non_central_court_application) do
+      create(:prior_authority_application,
+             created_at: 2.days.ago,
+                    data: build(:prior_authority_data, court_type: 'other'))
+    end
+
+    before do
+      central_court_application && non_central_court_application
+    end
+
+    it 'prefers the central criminal court case' do
+      expect(described_class.call(user)).to eq central_court_application
+    end
   end
 
-  it 'prefers central criminal to pathology court cases' do
-    central = create(:prior_authority_application,
-                     data: build(:prior_authority_data, service_type: 'other', court_type: 'central_criminal_court'))
-    create(:prior_authority_application,
-           data: build(:prior_authority_data, service_type: 'pathologist', court_type: 'other'))
-    expect(described_class.call(user)).to eq central
+  context 'when the newer application is a pathology case' do
+    let(:pathology_application) do
+      create(:prior_authority_application,
+             created_at: 1.day.ago,
+                    data: build(:prior_authority_data, service_type: 'pathologist'))
+    end
+
+    let(:non_pathology_application) do
+      create(:prior_authority_application,
+             created_at: 2.days.ago,
+                    data: build(:prior_authority_data, service_type: 'other'))
+    end
+
+    before do
+      pathology_application && non_pathology_application
+    end
+
+    it 'prefers the pathology case' do
+      expect(described_class.call(user)).to eq pathology_application
+    end
   end
 
-  it 'prefers earlier submisions' do
-    create(:prior_authority_application, created_at: 1.day.ago)
-    prior = create(:prior_authority_application, created_at: 2.days.ago)
-    expect(described_class.call(user)).to eq prior
+  context 'when there is a pathology case and a central court case' do
+    let(:pathology_application) do
+      create(:prior_authority_application,
+             created_at: 1.day.ago,
+                    data: build(:prior_authority_data, service_type: 'pathologist'))
+    end
+
+    let(:central_court_application) do
+      create(:prior_authority_application,
+             created_at: 2.days.ago,
+                    data: build(:prior_authority_data, court_type: 'central_criminal_court'))
+    end
+
+    before do
+      pathology_application && central_court_application
+    end
+
+    it 'prefers the central criminal court case' do
+      expect(described_class.call(user)).to eq central_court_application
+    end
   end
 end
--- spec/system/prior_authority/assign_applications_spec.rb
+++ spec/system/prior_authority/assign_applications_spec.rb
@@ -9,18 +9,26 @@ RSpec.describe 'Assign applications' do
     click_on 'Accept analytics cookies'
     visit prior_authority_root_path
     click_on 'Start now'
+    application
+    click_on 'Assess next application'
   end
 
-  it 'lets me assign an application to myself' do
-    application = create(:prior_authority_application)
-    click_on 'Assess next application'
-    # As the UI has not yet been built, the most we can do is demonstrate that an assignment has been made
-    expect(application.reload.assignments.first.user).to eq caseworker
-    expect(application.events.first).to be_an Event::Assignment
+  context 'when there is an application' do
+    let(:application) { create(:prior_authority_application) }
+
+    it 'lets me assign theapplication to myself' do
+      # As the UI has not yet been built, the most we can do is demonstrate that an assignment has been made
+      expect(application.reload.assignments.first.user).to eq caseworker
+      expect(application.events.first).to be_an Event::Assignment
+    end
   end
 
-  it 'copes if there is no application' do
-    click_on 'Assess next application'
-    expect(page).to have_content 'There are no applications waiting to be allocated'
+  context 'when there is no application' do
+    let(:application) { nil }
+
+    it 'shows me an explanation' do
+      click_on 'Assess next application'
+      expect(page).to have_content 'There are no applications waiting to be allocated'
+    end
   end
 end
