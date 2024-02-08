7d0d711ac98c7ef38daf70ee992300eeb50b1d0b
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2024-02-08 09:05:33 +0000

    CRM457-1055: Update to latest MOJ-frontend (#264)
    # Description of change
    Our importmap file got out of sync with our package.json
    
    [Link to relevant
    ticket](https://dsdmoj.atlassian.net/browse/CRM457-1055)
    
    # Notes for reviewer
    There will be a separate ticket to assess whether we really need both an
    importmap and a package.json

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,5 +8,5 @@ pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin "govuk-frontend", to: "https://ga.jspm.io/npm:govuk-frontend@5.0.0/dist/govuk/all.mjs"
-pin "@ministryofjustice/frontend", to: "https://ga.jspm.io/npm:@ministryofjustice/frontend@2.0.0/moj/all.js"
+pin "@ministryofjustice/frontend", to: "https://ga.jspm.io/npm:@ministryofjustice/frontend@2.1.0/moj/all.js"
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js"
