f43d5e2fae7d8f11caabe72f62220b690856d70f
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-02-27 16:27:44 +0000

    Switch to propshaft

--- app/assets/config/manifest.js
+++
@@ -1,7 +0,0 @@
-//= link_tree ../images
-//= link_tree ../builds
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
-//= link_tree ../../../node_modules/govuk-frontend/dist/govuk/assets
-//= link_tree ../../../node_modules/@ministryofjustice/frontend/moj/assets
-//= link govuk-frontend/dist/govuk/all.bundle.js
