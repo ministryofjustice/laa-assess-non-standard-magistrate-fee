7b2f09a0482652658fa30af92da1cbc22ec60793
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-03-07 15:58:02 +0000

    Fix missing save!

--- db/migrate/20240307154917_update_data_blobs.rb
+++ db/migrate/20240307154917_update_data_blobs.rb
@@ -5,6 +5,7 @@ class UpdateDataBlobs < ActiveRecord::Migration[7.1]
       application.data['custom_main_offence_name'] = application.data['main_offence']
       application.data['prison_id'] = 'custom'
       application.data['custom_prison_name'] = application.data['client_detained_prison']
+      application.save!
     end
   end
 end
