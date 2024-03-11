8cc7f540fad19e9f110d46ca625adbb422f81afe
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-03-11 17:08:24 +0000

    Create Base Feedback File for CRM4

---
+++ app/mailers/prior_authority/feedback_messages/feedback_base.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module Nsm
+  module FeedbackMessages
+    class FeedbackBase
+      def initialize(submission, comment = '')
+        @submission = submission
+        @comment = comment
+      end
+
+      def template
+        raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+      end
+
+      def contents
+        raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+      end
+
+      def recipient
+        @submission.data['submitter']['email']
+      end
+
+      protected
+
+      def defendant
+        @submission.data['defendant']
+      end
+
+      def defendant_name
+        "#{defendant['first_name']} #{defendant['last_name']}"
+      end
+
+      def maat_id
+        defendant['maat']
+      end
+
+      def case_reference
+        @submission.data['laa_reference']
+      end
+
+      def ufn
+        @submission.data['ufn']
+      end
+
+      def feedback_url
+        Rails.configuration.x.contact.feedback_url
+      end
+    end
+  end
+end
