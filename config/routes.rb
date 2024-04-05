7a619cc812bd12d4f6130f3a89a975e9ab5324c6
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-04-05 12:44:15 +0100

    Add utility endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -111,4 +111,8 @@ Rails.application.routes.draw do
     resources :auto_assignments, only: [:create]
     resources :downloads, only: :show
   end
+
+  if ENV.fetch("ENABLE_SYNC_TRIGGER_ENDPOINT", false) == "true"
+    get "sync", to: "sync#sync_all"
+  end
 end
