6b117491b90b4b1b8cb230863ad967d6e25451f5
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2024-04-18 12:10:24 +0100

    CRM457-1325: Fix key information card for prison law (#419)
    ## Description of change
    - Show UFN on card but only for prison law
    - When there is no main offence ID (i.e. prison law cases), do not try
    to work out the main offence name
    [Link to relevant
    ticket](https://dsdmoj.atlassian.net/browse/CRM457-1325)

--- spec/view_models/prior_authority/v1/application_details/key_information_card_spec.rb
+++ spec/view_models/prior_authority/v1/application_details/key_information_card_spec.rb
@@ -4,10 +4,9 @@ RSpec.describe PriorAuthority::V1::ApplicationDetails::KeyInformationCard do
   subject(:key_information) { described_class.new(application_summary) }
 
   let(:klass) { PriorAuthority::V1::KeyInformation }
+  let(:application_summary) { klass.new(args) }
 
   describe '#primary_quote_postcode' do
-    let(:application_summary) { klass.new(args) }
-
     let(:args) do
       {
         quotes: [
@@ -21,6 +20,40 @@ RSpec.describe PriorAuthority::V1::ApplicationDetails::KeyInformationCard do
 
     it 'returns the postcode from the quote marked as primary true' do
       expect(key_information.primary_quote_postcode).to eq('E15 3JQ')
+    end
+  end
+
+  describe '#ufn' do
+    let(:args) do
+      {
+        prison_law: false,
+        ufn: '121212/001'
+      }
+    end
+
+    it 'returns nil when not prison law' do
+      expect(key_information.ufn).to be_nil
+    end
+  end
+
+  context 'when it is a prison law case' do
+    let(:args) do
+      {
+        prison_law: true,
+        ufn: '121212/001'
+      }
+    end
+
+    describe '#main_offence' do
+      it 'returns nil' do
+        expect(key_information.main_offence).to be_nil
+      end
+    end
+
+    describe '#ufn' do
+      it 'returns the ufn' do
+        expect(key_information.ufn).to eq '121212/001'
+      end
     end
   end
 end
