32d33736432778a1ce5ddf2c24016b2d997b3cff
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2024-05-20 12:05:50 +0100

    CRM457-1467: Use rationals instead of decimals to deal with recurring decimal imprecision (#483)
    ## Description of change
    
    [Link to relevant
    ticket](https://dsdmoj.atlassian.net/browse/CRM457-1467)

--- app/view_models/nsm/v1/work_item.rb
+++ app/view_models/nsm/v1/work_item.rb
@@ -112,7 +112,10 @@ module Nsm
 
       def calculate_cost(original: false)
         scoped_uplift, scoped_time_spent = original ? [original_uplift, original_time_spent] : [uplift, time_spent]
-        pricing * scoped_time_spent * (100 + scoped_uplift.to_i) / 100 / 60
+        # We need to use a Rational because some numbers divided by 60 cannot be accurately represented as a decimal,
+        # and when summing up multiple work items with sub-penny precision, those small inaccuracies can lead to
+        # a larger inaccuracy when the total is eventually rounded to 2 decimal places.
+        Rational(pricing * scoped_time_spent * (100 + scoped_uplift.to_i), 100 * 60)
       end
 
       def format(value, as: :pounds)
