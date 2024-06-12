7aa2798e45f4588a59db8fde4993845428aec37d
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2024-06-12 17:27:07 +0100

    Adds no cov

--- app/validators/cost_item_type_dependant_validator.rb
+++ app/validators/cost_item_type_dependant_validator.rb
@@ -10,7 +10,9 @@ class CostItemTypeDependantValidator < ActiveModel::EachValidator
   end
 
   def cost_item_type_for(record)
+    # :nocov:
     basic = record.respond_to?(:cost_item_type) ? record.cost_item_type || 'item' : 'item'
     options[:pluralize] ? basic.pluralize : basic
+    # :nocov:
   end
 end
