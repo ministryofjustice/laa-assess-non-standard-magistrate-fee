6098ed415d566f6c9a8a10d4a5b5d873d54b48be
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2024-06-12 12:31:29 +0100

    Updates PA factory

--- spec/factories/prior_authority_applications.rb
+++ spec/factories/prior_authority_applications.rb
@@ -75,6 +75,7 @@ FactoryBot.define do
     initialize_with { attributes }
     id { SecureRandom.uuid }
     cost_type { 'per_item' }
+    cost_item_type { 'per_item' }
     cost_per_hour { nil }
     cost_multiplier { '1.0' }
     cost_per_item { '3.5' }
