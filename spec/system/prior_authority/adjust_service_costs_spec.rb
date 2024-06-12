ea4c785487f419346a855c18544c3aaa6a9fafe5
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2024-06-12 13:11:05 +0100

    Fixes rspec failures

--- spec/system/prior_authority/adjust_service_costs_spec.rb
+++ spec/system/prior_authority/adjust_service_costs_spec.rb
@@ -142,6 +142,7 @@ RSpec.describe 'Adjust service costs' do
               cost_type: 'per_item',
               item_type: 'minute',
               cost_item_type: 'minute',
+              cost_item_type: 'minute',
               items: 100,
               cost_per_item: '2.00',
             ),
