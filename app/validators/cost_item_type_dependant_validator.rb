c1b1b0059b20c4acba6feaa2c2c2f2ac647d1763
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-06-17 09:52:40 +0100

    Remove uneeded pluralize option

--- app/validators/cost_item_type_dependant_validator.rb
+++ app/validators/cost_item_type_dependant_validator.rb
@@ -10,7 +10,6 @@ class CostItemTypeDependantValidator < ActiveModel::EachValidator
   end
 
   def cost_item_type_for(record)
-    basic = record.respond_to?(:cost_item_type) ? record.cost_item_type || 'item' : 'item'
-    options[:pluralize] ? basic.pluralize : basic
+    record.respond_to?(:cost_item_type) ? record.cost_item_type || 'item' : 'item'
   end
 end
