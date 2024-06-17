f64c41a86ff6e7f06361e1a1475a2b5c5c2d6daf
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-06-17 11:10:04 +0100

    Address review comments

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,14 +2,17 @@ class DashboardsController < ApplicationController
   before_action :authorize_supervisor
 
   def show
-    payload = {
-      resource: { dashboard: ENV.fetch('METABASE_DASHBOARD_ID').to_i },
-      params: {},
-      exp: Time.now.to_i + (60 * 10) # 10 minute expiration
-    }
-    token = JWT.encode(payload, ENV.fetch('METABASE_SECRET_KEY'))
+    ids = ENV.fetch('METABASE_DASHBOARD_IDS').split(',')
+    @iframe_urls = ids.map do |id|
+      payload = {
+        resource: { dashboard: id.to_i },
+        params: {},
+        exp: Time.now.to_i + 10 # 10 second expiration to ensure the iframe has time to load
+      }
+      token = JWT.encode(payload, ENV.fetch('METABASE_SECRET_KEY'))
 
-    @iframe_url = "#{ENV.fetch('METABASE_SITE_URL')}/embed/dashboard/#{token}#bordered=true&titled=true"
+      "#{ENV.fetch('METABASE_SITE_URL')}/embed/dashboard/#{token}#bordered=true&titled=true"
+    end
   end
 
   def authorize_supervisor
