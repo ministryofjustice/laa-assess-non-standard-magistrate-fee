7ca6b3aece9c9fa5722db4b5606023d96f5e2964
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-06-18 11:36:59 +0100

    add additional tests

--- spec/system/dashboards_spec.rb
+++ spec/system/dashboards_spec.rb
@@ -1,22 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe 'Dashboards' do
-  context 'when I am not a supervisor' do
-    before { sign_in create(:caseworker) }
+  context 'when insights feature flag is enabled' do
+    before { allow(FeatureFlags).to receive(:insights).and_return(double(enabled?: true)) }
+    context 'when I am not a supervisor' do
+      before { sign_in create(:caseworker) }
 
-    it 'does not let me visit the dashboard path' do
-      visit dashboard_path
-      expect(page).to have_current_path(root_path)
+      it 'does not let me visit the dashboard path' do
+        visit dashboard_path
+        expect(page).to have_current_path(root_path)
+      end
+    end
+
+    context 'when I am a supervisor' do
+      before { sign_in create(:supervisor) }
+
+      it 'lets me visit the dashboard path' do
+        visit root_path
+        click_on 'View insights'
+        expect(page).to have_current_path(dashboard_path)
+      end
     end
   end
 
-  context 'when I am a supervisor' do
-    before { sign_in create(:supervisor) }
+  context 'when insights feature flag is disabled' do
+    before { allow(FeatureFlags).to receive(:insights).and_return(double(enabled?: false)) }
 
-    it 'lets me visit the dashboard path' do
-      visit root_path
-      click_on 'View insights'
-      expect(page).to have_current_path(dashboard_path)
+    context 'when I am a supervisor' do
+      before { sign_in create(:supervisor) }
+
+      it 'does not show link for insights' do
+        visit root_path
+        expect(page).not_to include('View insights')
+      end
     end
   end
 end
