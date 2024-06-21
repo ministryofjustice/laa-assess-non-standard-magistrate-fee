06d5d9dd34e44517e4b6de8d1c6872036340b6be
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2024-06-21 16:27:17 +0100

    add additional tests and fallback values for dashboard ids

--- spec/system/dashboards_spec.rb
+++ spec/system/dashboards_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Dashboards' do
   context 'when insights feature flag is enabled' do
     before do
-      allow(FeatureFlags).to receive(:insights).and_return(double(enabled?: true))
+      allow(ENV).to receive(:fetch).with('METABASE_PA_DASHBOARD_IDS')
+                                   .and_return('14,6')
+      allow(ENV).to receive(:fetch).with('METABASE_NSM_DASHBOARD_IDS')
+                                   .and_return('14')
+      allow(FeatureFlags).to receive(:insights_enabled).and_return(double(enabled?: true))
     end
 
     context 'when I am not a supervisor' do
@@ -22,6 +26,50 @@ RSpec.describe 'Dashboards' do
         visit root_path
         click_on 'View insights'
         expect(page).to have_current_path(dashboard_path)
+      end
+
+      it 'by default lets me view the prior authority analytics' do
+        title = find('.govuk-heading-xl')
+        expect(title).to have_text('Prior authority')
+
+        tab = find('.moj-primary-navigation__link')
+        expect(tab).to have_text('Prior authority')
+      end
+
+      context 'when nsm feature flag is enabled' do
+        before do
+          allow(FeatureFlags).to receive(:nsm_enabled).and_return(double(enabled?: true))
+          visit dashboard_path
+        end
+
+        it 'can navigate to nsm analytics' do
+          click_on 'Non-standard magistrates'
+          expect(page).to have_current_path(dashboard_path)
+
+          title = find('.govuk-heading-xl')
+          expect(title).to have_text('Non-standard magistrates')
+
+          tab = find('.moj-primary-navigation__link')
+          expect(tab).to have_text('Non-standard magistrates')
+        end
+      end
+
+      context 'when nsm feature flag is disabled' do
+        before do
+          allow(FeatureFlags).to receive(:nsm_enabled).and_return(double(enabled?: false))
+          visit dashboard_path
+        end
+
+        it 'does not have tab to navigate to nsm analytics' do
+          expect(page).not_to have_text('Non-standard magistrates')
+        end
+
+        it 'cannot navigate to nsm page' do
+          visit dashboard_path(service: 'nsm')
+
+          title = find('.govuk-heading-xl')
+          expect(title).to have_text('Non-standard magistrates')
+        end
       end
     end
   end
