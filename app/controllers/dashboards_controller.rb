66d8099cbee44ba0cd9a6594b26239396aa94996
Ivan Yohuno <ivan.yohuno@digital.justice.gov.uk>
2024-06-28 12:49:54 +0100

    Provider: Analytics Dashboard Tab (CRM4 & CRM7) (#574)
    ## Description of change
    
    [Provider: Analytics Dashboard Tab (CRM4 &
    CRM7)](https://dsdmoj.atlassian.net/browse/CRM457-1636)
    
    - [ ] Make dashboards 100% width within column
    - [ ] Add correct dashboards
    
    
    ## Screenshots of changes (if applicable)
    
    ### Before changes:
    
    ### After changes:

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -9,24 +9,10 @@ class DashboardsController < ApplicationController
   end
 
   def generate_dashboards(service)
-    if service == 'prior_authority'
-      ids = ENV.fetch('METABASE_PA_DASHBOARD_IDS')&.split(',')
-    elsif service == 'nsm'
-      ids = ENV.fetch('METABASE_NSM_DASHBOARD_IDS')&.split(',')
-    end
-
-    ids ||= []
-
-    @iframe_urls = ids.map do |id|
-      payload = {
-        resource: { dashboard: id.to_i },
-        params: {},
-        exp: Time.now.to_i + (60 * 60) # After an hour, using in-dashboard widgets will stop working
-      }
-      token = JWT.encode(payload, ENV.fetch('METABASE_SECRET_KEY'))
-
-      "#{ENV.fetch('METABASE_SITE_URL')}/embed/dashboard/#{token}#bordered=true&titled=true"
-    end
+    overview_ids = get_overview_ids(service)
+    @iframe_urls = generate_metabase_urls(overview_ids)
+    autogrant_ids = ENV.fetch('METABASE_PA_AUTOGRANT_DASHBOARD_IDS')&.split(',') || []
+    @autogrant_urls = generate_metabase_urls(autogrant_ids)
   end
 
   def service
@@ -36,5 +22,29 @@ class DashboardsController < ApplicationController
 
   def authorize_supervisor
     redirect_to root_path if !FeatureFlags.insights.enabled? || !current_user.supervisor?
+  end
+
+  private
+
+  def get_overview_ids(service)
+    if service == 'prior_authority'
+      ids = ENV.fetch('METABASE_PA_DASHBOARD_IDS')&.split(',')
+    elsif service == 'nsm'
+      ids = ENV.fetch('METABASE_NSM_DASHBOARD_IDS')&.split(',')
+    end
+    ids || []
+  end
+
+  def generate_metabase_urls(ids)
+    ids.map do |id|
+      payload = {
+        resource: { dashboard: id.to_i },
+        params: {},
+        exp: Time.now.to_i + (60 * 60) # After an hour, using in-dashboard widgets will stop working
+      }
+      token = JWT.encode(payload, ENV.fetch('METABASE_SECRET_KEY'))
+
+      "#{ENV.fetch('METABASE_SITE_URL')}/embed/dashboard/#{token}#bordered=true&titled=true"
+    end
   end
 end
