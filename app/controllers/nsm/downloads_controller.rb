056790778ad4ceca8a983213c8fb2a370f71f90a
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-07-11 16:42:00 +0100

    Extend character protection to NSM downloads

--- app/controllers/nsm/downloads_controller.rb
+++ app/controllers/nsm/downloads_controller.rb
@@ -1,17 +1,12 @@
 module Nsm
   class DownloadsController < Nsm::BaseController
-    # 1 min expiry on pre-signed urls to keep evidence download as secure as possible
-    PRESIGNED_EXPIRY = 60
+    include FileRedirectable
 
     def show
       claim = Claim.find(params[:claim_id])
       supporting_evidence = BaseViewModel.build(:supporting_evidence, claim, 'supporting_evidences')
       item = supporting_evidence.detect { _1.id == params[:id] }
-      download_url = S3_BUCKET.object(item.file_path)
-                              .presigned_url(:get,
-                                             expires_in: PRESIGNED_EXPIRY,
-                                             response_content_disposition: "attachment; filename=\"#{item.file_name}\"")
-      redirect_to download_url, allow_other_host: true
+      redirect_to_file_download(item.file_path, item.file_name)
     end
   end
 end
