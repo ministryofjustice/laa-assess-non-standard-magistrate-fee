cecd75e7be8b92d8d115899d3ec509bcd742fe4e
David Henry <david.henry@digital.justice.gov.uk>
2024-07-22 10:20:25 +0100

    typo

--- spec/system/accessibility_spec.rb
+++ spec/system/accessibility_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe 'Accessibility', :accessibility do
 
   context 'when viewing claim-specific screens' do
     %i[nsm_claim_claim_details
-       nsm_claim_review_and_adjust
+       nsm_claim_review_and_adjusts
        nsm_claim_adjustments
        nsm_claim_supporting_evidences
        nsm_claim_history
