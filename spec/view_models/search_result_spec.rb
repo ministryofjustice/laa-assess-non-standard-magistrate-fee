8ce5cb9737deb428fb9375e8488e9d24a0870cfa
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-07-23 09:33:19 +0100

    fix SearchResult tests

--- spec/view_models/search_result_spec.rb
+++ spec/view_models/search_result_spec.rb
@@ -2,23 +2,19 @@ require 'rails_helper'
 
 RSpec.describe SearchResult do
   describe '#application_path' do
-    let(:application_type) { nil }
-    let(:application_id) { nil }
-    let(:nsm_application) { create(:claim) }
-    let(:pa_application) { create(:prior_authority_application) }
-    let(:submission) do
-      {
-        application_type: application_type,
-        application_id: application_id,
-        version: 1
-      }
-    end
-
-    subject = described_class.new(submission)
-
+    application_id = SecureRandom.uuid
     context 'submission is nsm' do
-      let(:application_type) { 'crm7' }
-      let(:application_id) { nsm_application.id }
+      before do
+        create(:claim, id: application_id)
+      end
+
+      subject = described_class.new(
+        {
+          application_type: 'crm7',
+          application_id: application_id,
+          version: 1
+        }
+      )
 
       it 'generates a link to the non-standard magistrate details page' do
         expect(subject.application_path).to eq("/nsm/claims/#{application_id}/claim_details")
@@ -26,8 +22,17 @@ RSpec.describe SearchResult do
     end
 
     context 'submission is prior authority' do
-      let(:application_type) { 'crm4' }
-      let(:application_id) { pa_application.id }
+      before do
+        create(:prior_authority_application, id: application_id)
+      end
+
+      subject = described_class.new(
+        {
+          application_type: 'crm4',
+          application_id: application_id,
+          version: 1
+        }
+      )
 
       it 'generates a link to the prior authority details page' do
         expect(subject.application_path).to eq("/prior_authority/applications/#{application_id}")
