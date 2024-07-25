3f9452d40f7d6bea9072f2b037a2c65fdd183ff0
Ellis Kenyo <ellis.kenyo@digital.justice.gov.uk>
2024-07-25 17:23:06 +0100

    Ensure that the back link_to is handled when going back
    Because of the Content-Security-Policy, the default behaviour[1] of
    `link_to :back` can't be used, so we simulate the behaviour instead.
    
    Any anchor elements with the id of "back-button" will trigger a
    history.back call.
    
    [1]
    https://api.rubyonrails.org/classes/ActionView/Helpers/UrlHelper.html#method-i-link_to

--- app/javascript/lib/table-item-anchors.js
+++ app/javascript/lib/table-item-anchors.js
@@ -1,12 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
+  [...document.querySelectorAll("a#back-button")].map(function (btn) {
+    btn.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      history.back();
+    });
+    return btn;
+  });
   [...document.querySelectorAll("table.govuk-table tr[id] a")].map(
     function (link) {
       link.addEventListener("click", function () {
         const nearestRow = link.closest("tr");
         if (nearestRow) {
-          history.replaceState({}, "", `#${nearestRow.id}`);
+          history.replaceState(history.state, "", `#${nearestRow.id}`);
         }
       });
+      return link;
     },
   );
 });
