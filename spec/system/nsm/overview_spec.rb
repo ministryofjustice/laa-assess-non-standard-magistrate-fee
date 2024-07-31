4371a382bbd25786aad10a9d7701d33694e391f9
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2024-07-31 09:34:09 +0100

    Add system spec for part granted claims
    Excercise fundamentals of page to cover regresions. This came
    out of change of routes when removing turboframes which did
    not replace all path helpers.

--- spec/system/nsm/overview_spec.rb
+++ spec/system/nsm/overview_spec.rb
@@ -7,12 +7,14 @@ RSpec.describe 'Overview', type: :system do
   before do
     sign_in user
     create(:assignment, submission: claim, user: user)
-    visit nsm_claim_claim_details_path(claim)
   end
 
   context 'when claim has been submitted' do
+    before { visit nsm_claim_claim_details_path(claim) }
+
     it 'shows me the total claimed but not adjusted' do
-      expect(page).to have_content('Claimed: £325.97')
+      expect(page)
+        .to have_content('Claimed: £325.97')
         .and have_no_content('Allowed:')
     end
   end
@@ -27,17 +29,38 @@ RSpec.describe 'Overview', type: :system do
     end
 
     it 'shows me the total claimed and total adjusted' do
-      expect(page).to have_content('Claimed: £325.97')
+      expect(page)
+        .to have_content('Claimed: £325.97')
         .and have_content('Allowed: £225.97')
     end
   end
 
-  context 'when claim has been assessed' do
+  context 'when claim has been assessed as granted' do
+    before { visit nsm_claim_claim_details_path(claim) }
+
     let(:claim) { create(:claim, state: 'granted') }
 
     it 'shows me the total claimed and total adjusted' do
-      expect(page).to have_content('Claimed: £325.97')
+      expect(page)
+        .to have_content('Claimed: £325.97')
         .and have_content('Allowed: £325.97')
+    end
+  end
+
+  context 'when claim has been assessed as part granted' do
+    let(:claim) { create(:claim, state: 'part_grant') }
+    let(:part_grant_event) { create(:event, :part_granted, claim:) }
+
+    before do
+      part_grant_event
+      visit nsm_claim_claim_details_path(claim)
+    end
+
+    it 'shows me the decision, comment and review adjustments link' do
+      expect(page)
+        .to have_selector('strong', text: 'Part granted')
+        .and have_content(part_grant_event.details['comment'])
+        .and have_link('Review quote adjustments')
     end
   end
 end
