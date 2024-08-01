aa7082768d0a91a3722581d8eb38c7d39b047f44
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-08-01 15:59:48 +0100

    add migration for backfill
    (literally the same code from app store)

---
+++ db/migrate/20240801144635_backfill_last_updated_at_in_submission.rb
@@ -0,0 +1,31 @@
+class BackfillLastUpdatedAtInSubmission < ActiveRecord::Migration[7.1]
+  disable_ddl_transaction!
+
+  #migration code taken from BackfillLastUpdatedAt in App Store
+  def up
+    Submission.unscoped.each do |submission|
+      # Update all last_updated_at columns to be the latest event date
+      # or, if no events exist, then the latest application version created_at date.
+      #
+
+      # default to created_at of current verions
+      last_updated_at = submission.latest_version.created_at
+
+      # Events may not exist or dirty data means some may not have certain expected fields
+      if submission.events
+        usable_events = submission.events.select {|ev| ev['created_at'].present? }
+
+        if usable_events.any?
+          latest_event = usable_events.max_by { |ev| ev["created_at"].to_time }
+          last_updated_at = latest_event['created_at'].to_time
+        end
+      end
+
+      submission.update_columns(last_updated_at:)
+
+      sleep(0.01) # throttle
+    rescue StandardError => e
+      Rails.logger.info("Backfill of last_updated_at failed with #{e}")
+    end
+  end
+end
