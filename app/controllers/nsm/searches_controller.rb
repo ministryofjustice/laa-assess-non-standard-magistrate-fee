da6748c500ecd5f7f731c4418f8b9dde9d7855cf
robin skilling <robin.skilling@digital.justice.gov.uk>
2024-08-07 15:13:38 +0100

    scoped nsm search controller

---
+++ app/controllers/nsm/searches_controller.rb
@@ -0,0 +1,36 @@
+module Nsm
+  class SearchesController < BaseController
+    def show
+      @search_form = SearchForm.new(search_params)
+      @search_form.execute if @search_form.valid?
+    end
+
+    def new
+      @search_form = SearchForm.new(default_params)
+      render :show
+    end
+
+    private
+
+    def search_params
+      params.require(:search_form).permit(
+        :query,
+        :submitted_from,
+        :submitted_to,
+        :updated_from,
+        :updated_to,
+        :status_with_assignment,
+        :caseworker_id,
+        :sort_by,
+        :sort_direction
+      ).merge(default_params)
+    end
+
+    def default_params
+      {
+        application_type: Submission::APPLICATION_TYPES[:nsm],
+        page: params.fetch(:page, '1')
+      }
+    end
+  end
+end
