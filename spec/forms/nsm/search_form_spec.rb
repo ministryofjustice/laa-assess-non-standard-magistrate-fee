6db3cb49ae426f935516988f8d5fe6c884305b66
robin skilling <robin.skilling@digital.justice.gov.uk>
2024-08-13 16:59:17 +0100

    scope form to application type

---
+++ spec/forms/nsm/search_form_spec.rb
@@ -0,0 +1,13 @@
+
+require 'rails_helper'
+
+RSpec.describe Nsm::SearchForm do
+  subject { described_class.new }
+
+  describe '#statuses' do
+    it 'does not contain auto_grant status' do
+      statuses = subject.statuses.map(&:value)
+      refute(statuses.include?(:auto_granted))
+    end
+  end
+end
