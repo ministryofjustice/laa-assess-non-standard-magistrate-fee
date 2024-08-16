fbbc6a36ac242b23ba94f436472b207572fd115b
robin skilling <robin.skilling@digital.justice.gov.uk>
2024-08-16 18:19:16 +0100

    add some tests

--- spec/models/claims_spec.rb
+++ spec/models/claims_spec.rb
@@ -44,4 +44,29 @@ RSpec.describe Claim do
       expect(claim).to be_granted
     end
   end
+
+  describe '#formatted_allowed_total' do
+    context 'granted' do
+      it 'gross_cost if claim less than allowed' do
+        allowed_more = create(:claim, :increase_adjustment)
+        allowed_more.state = Nsm::MakeDecisionForm::GRANTED
+
+        expect(allowed_more.formatted_allowed_total).to eq '£355.97'
+      end
+    end
+
+    context 'rejected' do
+      it '£0.00 for rejected claims' do
+        claim.state = Nsm::MakeDecisionForm::REJECTED
+        expect(claim.formatted_allowed_total).to eq '£0.00'
+      end
+    end
+
+    context 'part_grant' do
+      it 'allowed_gross_cost' do
+        claim.state = Nsm::MakeDecisionForm::PART_GRANT
+        expect(claim.formatted_allowed_total).to eq '£325.97'
+      end
+    end
+  end
 end
