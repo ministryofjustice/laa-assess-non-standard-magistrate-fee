ca65e0be0d3448681e92f775eb375b775442bca2
Ivan Yohuno <ivan.yohuno@digital.justice.gov.uk>
2024-08-16 16:50:29 +0100

    Add tasks for secret rotation (#691)
    ## Description of change
    
    [MVP - Set up rotation of RDS and Elasticache
    secrets](https://dsdmoj.atlassian.net/browse/CRM457-1716)
    
    ## Notes for reviewer
    1. db:connection:check_connection - Checks if ActiveRecord can connect
    to DB and outputs results
    2. redis_sidekiq:check_connection - Checks if SidekiqServer and
    SidekiqClient can connect to redis server
    3. redis_sidekiq:retry_dead_jobs[:days_from_now] - Finds dead jobs (via
    Sidekiq::DeadSet) and retries all dead jobs from now to days_from_now
    ago - reporting which jobs got retried, which failed to retry and a
    summary of # of jobs in the dead queue and # of jobs retried (if these
    numbers don't match - investigation needed to find out why)
    These jobs related to scripts to help do checks for [Rotating DB
    Passwords](https://dsdmoj.atlassian.net/wiki/spaces/CRM457/pages/4985619054/Secrets#Steps-for-rotating-database-password)
    and [Rotating Redis
    Auth](https://dsdmoj.atlassian.net/wiki/spaces/CRM457/pages/4985619054/Secrets#How-to-rotate-Elasticache-Auth-Credentials-(WIP))

---
+++ spec/lib/tasks/redis_sidekiq_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+describe 'redis_sidekiq:', type: :task do
+  describe 'retry_dead_jobs' do
+    subject { Rake::Task['redis_sidekiq:retry_dead_jobs'] }
+
+    let(:days_from) { '1' }
+    let(:expected_output) { nil }
+
+    before do
+      Rails.application.load_tasks if Rake::Task.tasks.empty?
+    end
+
+    after do
+      Rake::Task['redis_sidekiq:retry_dead_jobs'].reenable
+    end
+
+    context 'valid value for days_from_now arg' do
+      let(:expected_output) { "Retried job with jid: 1\nFailed to retry job with jid: 2\n2 job(s) found\n1 job(s) retried" }
+
+      before do
+        dead_jobs = [
+          instance_double(Sidekiq::SortedEntry, at: DateTime.now, retry: true, jid: '1'),
+          instance_double(Sidekiq::SortedEntry, at: DateTime.now, retry: false, jid: '2'),
+          instance_double(Sidekiq::SortedEntry, at: 2.days.ago, retry: true, jid: '3')
+        ]
+        allow(Sidekiq::DeadSet).to receive(:new).and_return(dead_jobs)
+      end
+
+      it 'accurately reports jobs retried' do
+        expect { Rake::Task['redis_sidekiq:retry_dead_jobs'].invoke(days_from) }.to output(expected_output).to_stdout
+      end
+    end
+
+    context 'inputs invalid value for days_from_now arg' do
+      let(:days_from) { 'garbage' }
+      let(:expected_output) { 'You must enter a valid integer greater than 0' }
+
+      it 'prints out an error' do
+        expect { Rake::Task['redis_sidekiq:retry_dead_jobs'].invoke(days_from) }.to output(expected_output).to_stdout
+      end
+    end
+  end
+end
