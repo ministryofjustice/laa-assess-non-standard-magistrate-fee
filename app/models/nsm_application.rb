09b155fa034884c468e49d3ca238e57b0a70061c
robin skilling <robin.skilling@digital.justice.gov.uk>
2024-09-06 12:31:12 +0100

    claim is NSM

--- app/models/nsm_application.rb
+++
@@ -1,7 +0,0 @@
-class NsmApplication < BaseApplication
-  default_scope -> { where(application_type: APPLICATION_TYPES[:nsm]) }
-
-  scope :related_applications, lambda { |ufn, account_number|
-    Nsm::RelatedApplications.call(ufn, account_number)
-  }
-end
