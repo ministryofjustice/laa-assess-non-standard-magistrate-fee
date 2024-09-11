6765a7fb831a1dd00967b708e6800b22fecc01e2
robin skilling <robin.skilling@digital.justice.gov.uk>
2024-09-11 17:25:17 +0100

    fix disbursement adjustment deletion confirm page

--- app/controllers/nsm/disbursement_adjustments_controller.rb
+++ app/controllers/nsm/disbursement_adjustments_controller.rb
@@ -1,8 +1,11 @@
 module Nsm
   class DisbursementAdjustmentsController < Nsm::BaseController
     def confirm_deletion
-      render 'nsm/shared/confirm_delete_adjustment',
-             locals: { item_name: t('.disbursement'),
+      @adjustment = BaseViewModel
+                    .build(:disbursement, claim, 'disbursements')
+                    .filter(&:any_adjustments?).find { _1.id == params[:id] }
+      render 'nsm/disbursements/confirm_delete_adjustment',
+             locals: { cost_item_type: t('.disbursement'),
                        deletion_path: nsm_claim_disbursement_adjustment_path(params[:claim_id],
                                                                              params[:id]),
                        nsm_adjustments_path: adjusted_nsm_claim_disbursements_path }
@@ -11,6 +14,12 @@ module Nsm
     def destroy
       Nsm::AdjustmentDeleter.new(params, :disbursement, current_user).call
       redirect_to adjusted_nsm_claim_disbursements_path(params[:claim_id])
+    end
+
+    private
+
+    def claim
+      @claim ||= Claim.find(params[:claim_id])
     end
   end
 end
