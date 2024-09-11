1762c41aedc1f43ef04d69ce28c649c0eeb8d1d2
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-09-11 17:48:11 +0100

    fix rspec issues

--- spec/lib/tasks/fixes_spec.rb
+++ spec/lib/tasks/fixes_spec.rb
@@ -45,8 +45,10 @@ describe 'fixes:', type: :task do
   end
 
   describe 'fix_corrupt_versions' do
-    let(:affected_submission) { create(:prior_authority_application, id: '84fabfe2-844f-4bbe-8460-1be4a18912e3', current_version: 3) }
-    let(:unaffected_submission) { create(:prior_authority_application, current_version: 3)}
+    let(:affected_submission) do
+      create(:prior_authority_application, id: '84fabfe2-844f-4bbe-8460-1be4a18912e3', current_version: 3)
+    end
+    let(:unaffected_submission) { create(:prior_authority_application, current_version: 3) }
 
     before do
       affected_submission
@@ -55,16 +57,16 @@ describe 'fixes:', type: :task do
     end
 
     after do
-      Rake::Task["fixes:fix_corrupt_versions"].reenable
+      Rake::Task['fixes:fix_corrupt_versions'].reenable
     end
 
     it 'does not decrement unaffected submission' do
-      Rake::Task["fixes:fix_corrupt_versions"].execute
+      Rake::Task['fixes:fix_corrupt_versions'].execute
       expect { unaffected_submission.current_version }.to eq(3)
     end
 
     it 'does decrement affected submission' do
-      Rake::Task["fixes:fix_corrupt_versions"].execute
+      Rake::Task['fixes:fix_corrupt_versions'].execute
       expect { affected_submission.current_version }.to eq(2)
     end
   end
