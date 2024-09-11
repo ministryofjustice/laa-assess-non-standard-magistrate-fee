89fe3c665fc02e2c52ceb8d37678c22c4a890eef
robin skilling <robin.skilling@digital.justice.gov.uk>
2024-09-11 17:47:23 +0100

    add work items

--- app/controllers/nsm/work_item_adjustments_controller.rb
+++ app/controllers/nsm/work_item_adjustments_controller.rb
@@ -1,9 +1,11 @@
 module Nsm
   class WorkItemAdjustmentsController < Nsm::BaseController
     def confirm_deletion
-      render 'nsm/shared/confirm_delete_adjustment',
-             locals: { item_name: t('.work_item'),
-                       deletion_path: nsm_claim_work_item_adjustment_path(params[:claim_id],
+      @adjustment = BaseViewModel
+                    .build(:work_item, claim, 'work_items')
+                    .filter(&:any_adjustments?).find { _1.id == params[:id] }
+      render 'nsm/work_items/confirm_delete_adjustment',
+             locals: { deletion_path: nsm_claim_work_item_adjustment_path(params[:claim_id],
                                                                           params[:id]),
                        nsm_adjustments_path: adjusted_nsm_claim_work_items_path }
     end
@@ -11,6 +13,12 @@ module Nsm
     def destroy
       Nsm::AdjustmentDeleter.new(params, :work_item, current_user).call
       redirect_to adjusted_nsm_claim_work_items_path(params[:claim_id])
+    end
+
+    private
+
+    def claim
+      @claim ||= Claim.find(params[:claim_id])
     end
   end
 end
