2f053ab523639fe746a959c97b407f8b539f2953
robin skilling <robin.skilling@digital.justice.gov.uk>
2024-09-12 14:48:14 +0100

    letters and calls confirm delete adjustment

--- app/controllers/nsm/letters_and_calls_adjustments_controller.rb
+++ app/controllers/nsm/letters_and_calls_adjustments_controller.rb
@@ -1,7 +1,11 @@
 module Nsm
   class LettersAndCallsAdjustmentsController < Nsm::BaseController
     def confirm_deletion
-      render 'nsm/shared/confirm_delete_adjustment',
+      @adjustment = BaseViewModel
+                    .build(:letter_and_call, claim, 'letters_and_calls')
+                    .filter(&:any_adjustments?)
+                    .find { _1.type.values['value'] == params[:id] }
+      render 'nsm/letters_and_calls/confirm_delete_adjustment',
              locals: { item_name: t('.letters_and_calls'),
                        deletion_path: nsm_claim_letters_and_calls_adjustment_path(params[:claim_id],
                                                                                   params[:id]),
@@ -11,6 +15,12 @@ module Nsm
     def destroy
       Nsm::AdjustmentDeleter.new(params, :letters_and_calls, current_user).call
       redirect_to adjusted_nsm_claim_letters_and_calls_path(params[:claim_id])
+    end
+
+    private
+
+    def claim
+      @claim ||= Claim.find(params[:claim_id])
     end
   end
 end
