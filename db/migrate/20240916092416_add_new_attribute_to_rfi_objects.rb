dee57988d22cd5bee3510bbd8f01bf2d4154be80
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-09-16 10:52:55 +0100

    Backfill

---
+++ db/migrate/20240916092416_add_new_attribute_to_rfi_objects.rb
@@ -0,0 +1,25 @@
+class AddNewAttributeToRfiObjects < ActiveRecord::Migration[7.1]
+  def up
+    PriorAuthorityApplication.provider_updated.find_each do |paa|
+      latest_provider_update_event = paa.events
+                                        .where(event_type: 'Event::ProviderUpdated')
+                                        .order(:created_at)
+                                        .last
+
+      added_fi = latest_provider_update_event.details['comment'].present?
+      added_correction = latest_provider_update_event.details['corrected_info'].any?
+
+      if added_fi
+        latest_fi = paa.data['further_information']&.max_by { _1['requested_at'] }
+        latest_fi && latest_fi['new'] = true
+      end
+
+      if added_correction
+        latest_correction = paa.data['incorrect_information']&.max_by { _1['requested_at'] }
+        latest_correction && latest_correction['new'] = true
+      end
+
+      paa.save!
+    end
+  end
+end
