373f355bf313c1d939c67f8092e8154f6cc7a266
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-09-17 17:29:50 +0100

    Remove maintenance_mode feature flag

--- spec/system/maintenance_mode_spec.rb
+++ spec/system/maintenance_mode_spec.rb
@@ -11,18 +11,4 @@ RSpec.describe 'Maintenance mode' do
       expect(page).to have_content 'Sorry, the service is unavailable'
     end
   end
-
-  context 'when maintenance mode is enabled via feature flag' do
-    let(:mode) { instance_double(FeatureFlags::EnabledFeature, enabled?: true) }
-
-    before do
-      allow(FeatureFlags).to receive(:maintenance_mode).and_return(mode)
-      ENV['MAINTENANCE_MODE'] = 'true'
-    end
-
-    it 'shows the maintenance screen on all URLS' do
-      visit closed_nsm_claims_path
-      expect(page).to have_content 'Sorry, the service is unavailable'
-    end
-  end
 end
