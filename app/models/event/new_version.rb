2fd9665eb212762a649987d3e2a33484e75f59f5
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-10-18 12:15:02 +0100

    Add comment

--- app/models/event/new_version.rb
+++ app/models/event/new_version.rb
@@ -1,6 +1,8 @@
 class Event
   class NewVersion < Event
     def self.build(submission:)
+      # We notify the app store immediately of a new version, because its analytics mechanism
+      # depends on having new version events available.
       create(submission: submission, submission_version: submission.current_version).tap(&:notify)
     end
 
