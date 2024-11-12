085b771ab80f1b8ad17723721409cfd4042225f5
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2024-11-12 16:05:23 +0000

    Hand off some responsibility to the app store

--- app/models/event/expiry.rb
+++ app/models/event/expiry.rb
@@ -1,7 +1,4 @@
 class Event
   class Expiry < Event
-    def self.construct(submission:)
-      create(submission: submission, submission_version: submission.current_version)
-    end
   end
 end
--- app/services/update_submission.rb
+++ app/services/update_submission.rb
@@ -13,29 +13,11 @@ class UpdateSubmission
   end
 
   def save
-    version_changed = @record['version'] > (submission.current_version || 0)
-
-    assign_attributes
-
-    # performed here to avoid slow transactions as requires API call to the OS API
-    cached_autograntable = autograntable?
-
     PriorAuthorityApplication.transaction do
       update_submission
-      # ensure new_version happens before the Autogrant
-      Event::NewVersion.build(submission:) if new_version_appropriate? && version_changed
-      autogrant if cached_autograntable
     end
 
-    NotifyAppStore.perform_now(submission:) if cached_autograntable
-
     submission
-  end
-
-  def new_version_appropriate?
-    # If this is a prior authority submission, only add a new version event on the first new version
-    @record['version'] == 1 ||
-      (@record['application_type'] == Submission::APPLICATION_TYPES[:nsm] && @record['application_state'] == 'provider_updated')
   end
 
   def assign_attributes
@@ -45,29 +27,12 @@ class UpdateSubmission
 
   private
 
-  def autograntable?
-    # performed here to avoid slow transactions as requires API call to the OS API
-    Autograntable.new(submission:).grantable?
-  rescue LocationService::NotFoundError, LocationService::UnknowablePartialPostcode
-    false
-  rescue LocationService::LocationError => e
-    Sentry.capture_exception(e)
-    false
-  end
-
   def update_submission
+    assign_attributes
     submission.save!
 
     @record['events']&.each do |event|
       submission.events.rehydrate!(event, submission.application_type)
     end
-  end
-
-  def autogrant
-    previous_state = submission.state
-    submission.data.merge!('updated_at' => Time.current, 'status' => PriorAuthorityApplication::AUTO_GRANT)
-    submission.update!(state: PriorAuthorityApplication::AUTO_GRANT)
-
-    Event::AutoDecision.build(submission:, previous_state:)
   end
 end
