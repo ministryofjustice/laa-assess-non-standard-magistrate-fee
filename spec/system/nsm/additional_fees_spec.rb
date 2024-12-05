33630c4bfb67e6cf274e0286e67cc4228de629ab
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-12-05 19:09:17 +0000

    add test for when there is an additional fee

--- spec/system/nsm/additional_fees_spec.rb
+++ spec/system/nsm/additional_fees_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 RSpec.describe 'Additional Fees', :stub_oauth_token do
   let(:user) { create(:caseworker) }
   let(:data) do
-    { youth_court:, claim_type:, plea_category:, rep_order_date: }
+    {
+      youth_court:, claim_type:, plea_category:, rep_order_date:,
+      include_youth_court_fee:, include_youth_court_fee_original:
+    }
   end
   let(:youth_court) { 'yes' }
   let(:claim_type) { 'non_standard_magistrate' }
+  let(:include_youth_court_fee_original) { nil }
+  let(:include_youth_court_fee) { false }
   let(:plea_category) { 'category_1a' }
   let(:rep_order_date) { Date.new(2024, 12, 6) }
   let(:state) { 'granted' }
@@ -33,7 +38,6 @@ RSpec.describe 'Additional Fees', :stub_oauth_token do
 
   context 'when claim is valid' do
     let(:state) { 'granted' }
-    let(:youth_court) { 'yes' }
 
     it 'lets me view details instead of changing them' do
       visit nsm_claim_additional_fees_path(claim)
@@ -42,6 +46,20 @@ RSpec.describe 'Additional Fees', :stub_oauth_token do
       expect(page).to have_content(
         'Youth court fee' \
         'Net cost claimed£0.00' \
+      )
+    end
+  end
+
+  context 'when a claim has an additional fee' do
+    let(:include_youth_court_fee) { true }
+
+    it 'shows the additional fee and what is allowed' do
+      visit nsm_claim_additional_fees_path(claim)
+      click_on 'Youth court fee'
+
+      expect(page).to have_content(
+        'Youth court fee' \
+        'Net cost claimed£598.59' \
       )
     end
   end
