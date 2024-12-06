37b4c5ac27f5673a63a00e8cac2d058dcd15734d
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-12-06 14:34:43 +0000

    Comment for adjustment ticket

--- app/view_models/nsm/v1/additional_fees_summary.rb
+++ app/view_models/nsm/v1/additional_fees_summary.rb
@@ -5,6 +5,9 @@ module Nsm
 
       def rows
         @rows ||= submission.additional_fees.map do |type, details|
+          # TODO: CRM457-2306 ensure adjustment comment
+          # attribute is instantiated on AdditionalFee instance
+          # so that any_adjustments? works as expected
           AdditionalFee.new({ type: }.merge(details))
         end
       end
