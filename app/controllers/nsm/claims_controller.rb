109d61c919fe3d1dc0344114bda59a6c01b90622
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-01-09 15:57:35 +0000

    add basic example of controller_params

--- app/controllers/nsm/claims_controller.rb
+++ app/controllers/nsm/claims_controller.rb
@@ -8,14 +8,14 @@ module Nsm
       return redirect_to open_nsm_claims_path unless policy(Claim).assign?
 
       @current_section = :your
-      model = Nsm::V1::YourClaims.new(params.permit(:page, :sort_by, :sort_direction).merge(current_user:))
+      model = Nsm::V1::YourClaims.new(controller_params.permit(:page, :sort_by, :sort_direction).merge(current_user:))
       model.execute
       render locals: { your_claims: model.results, pagy: model.pagy }
     end
 
     def open
       @current_section = :open
-      model = Nsm::V1::OpenClaims.new(params.permit(:page, :sort_by, :sort_direction))
+      model = Nsm::V1::OpenClaims.new(controller_params.permit(:page, :sort_by, :sort_direction))
       model.execute
       @pagy = model.pagy
       @claims = model.results
@@ -23,7 +23,7 @@ module Nsm
 
     def closed
       @current_section = :closed
-      model = Nsm::V1::ClosedClaims.new(params.permit(:page, :sort_by, :sort_direction))
+      model = Nsm::V1::ClosedClaims.new(controller_params.permit(:page, :sort_by, :sort_direction))
       model.execute
       @pagy = model.pagy
       @claims = model.results
@@ -42,12 +42,27 @@ module Nsm
       end
     end
 
+    def controller_params
+      safe_params = params.permit(
+        :id,
+        :sort_by,
+        :sort_direction,
+        :page
+      )
+      param_model = Nsm::ControllerParams::Claims.new(safe_params)
+      if param_model.valid?
+        safe_params
+      else
+        raise "#{param_model.error_summary}"
+      end
+    end
+
     private
 
     def set_default_table_sort_options
       default = 'date_updated'
-      @sort_by = params.fetch(:sort_by, default)
-      @sort_direction = params.fetch(:sort_direction, 'descending')
+      @sort_by = controller_params.fetch(:sort_by, default)
+      @sort_direction = controller_params.fetch(:sort_direction, 'descending')
     end
 
     def authorize_list
@@ -55,7 +70,7 @@ module Nsm
     end
 
     def submission_id
-      params[:id]
+      controller_params[:id]
     end
 
     def secondary_id
